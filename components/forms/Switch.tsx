49f36908e8de353429b164f5dacd385507f93d55
tjs <tjshipe@gmail.com>
2022-08-01 15:23:46 -0400

    slightly smaller switch

--- components/forms/Switch.tsx
+++ components/forms/Switch.tsx
@@ -26,7 +26,7 @@ const Switch: FunctionComponent<SwitchProps> = ({
         type="button"
         className={`${
           checked ? 'bg-th-primary' : 'bg-th-bkg-button'
-        } relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full 
+        } relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer rounded-full 
         border-2 border-transparent transition-colors duration-200 ease-in-out 
         focus:outline-none ${disabled ? 'opacity-60' : ''}`}
         role="switch"
@@ -39,7 +39,7 @@ const Switch: FunctionComponent<SwitchProps> = ({
           aria-hidden="true"
           className={`${
             checked ? 'translate-x-5' : 'translate-x-0'
-          } pointer-events-none inline-block h-5 w-5 transform rounded-full 
+          } pointer-events-none inline-block h-4 w-4 transform rounded-full 
           bg-white shadow ring-0 transition duration-200 ease-in-out`}
         ></span>
       </button>
