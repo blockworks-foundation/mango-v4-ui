f3f47a102e5487435d37f5eeab1ba1f3342a7ddc
tjs <tjshipe@gmail.com>
2022-08-23 14:13:30 -0400

    add init health to account summary box

--- components/account/MangoAccountSummary.tsx
+++ components/account/MangoAccountSummary.tsx
@@ -46,10 +46,19 @@ const MangoAccountSummary = () => {
           </p>
         </div>
         <div className="flex items-center justify-between">
-          <p className="text-sm text-th-fgd-3">{t('health')}</p>
+          <p className="text-sm text-th-fgd-3">Maint {t('health')}</p>
           <p className="text-sm font-bold text-th-fgd-1">
             {mangoAccount
               ? mangoAccount.getHealthRatioUi(HealthType.maint)
+              : 100}
+            %
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          <p className="text-sm text-th-fgd-3">Init {t('health')}</p>
+          <p className="text-sm font-bold text-th-fgd-1">
+            {mangoAccount
+              ? mangoAccount.getHealthRatioUi(HealthType.init)
               : 100}
             %
           </p>
