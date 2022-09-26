c06d0247c9f5bfc0d3779ca9f06e656b0d18a094
saml33 <slam.uke@gmail.com>
2022-09-26 13:03:26 +1000

    add market select logos

--- components/trade/AdvancedMarketHeader.tsx
+++ components/trade/AdvancedMarketHeader.tsx
@@ -1,9 +1,13 @@
 import { Serum3Market } from '@blockworks-foundation/mango-v4'
 import PercentageChange from '@components/shared/PercentageChange'
 import { Popover } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import {
+  ChevronDownIcon,
+  QuestionMarkCircleIcon,
+} from '@heroicons/react/20/solid'
 import mangoStore from '@store/mangoStore'
 import { useTranslation } from 'next-i18next'
+import Image from 'next/image'
 import { useCallback, useMemo } from 'react'
 import { DEFAULT_MARKET_NAME } from 'utils/constants'
 import { formatFixedDecimals } from 'utils/numbers'
@@ -11,6 +15,7 @@ import { formatFixedDecimals } from 'utils/numbers'
 const MarketSelectDropdown = () => {
   const selectedMarket = mangoStore((s) => s.selectedMarket.current)
   const serumMarkets = mangoStore((s) => s.serumMarkets)
+  const jupiterTokens = mangoStore((s) => s.jupiterTokens)
   const set = mangoStore((s) => s.set)
 
   const handleSelectMarket = useCallback(
@@ -23,11 +28,27 @@ const MarketSelectDropdown = () => {
     [set]
   )
 
+  const [baseLogoURI, quoteLogoURI] = useMemo(() => {
+    if (jupiterTokens.length && selectedMarket) {
+      const tokenSymbols = selectedMarket.name.split('/')
+      const baseURI = jupiterTokens.find(
+        (t) => t.symbol === tokenSymbols[0]
+      )!.logoURI
+      const quoteURI = jupiterTokens.find(
+        (t) => t.symbol === tokenSymbols[1]
+      )!.logoURI
+      return [baseURI, quoteURI]
+    } else {
+      return ['', '']
+    }
+  }, [jupiterTokens, selectedMarket])
+
   return (
     <Popover>
       {({ close, open }) => (
         <div className="relative flex flex-col overflow-visible">
-          <Popover.Button className="default-transition flex w-full items-center hover:text-th-primary">
+          <Popover.Button className="default-transition flex w-full items-center justify-between hover:text-th-primary">
+            <MarketLogos baseURI={baseLogoURI} quoteURI={quoteLogoURI} />
             <div className="text-xl font-bold md:text-base">
               {selectedMarket?.name || DEFAULT_MARKET_NAME}
             </div>
@@ -40,20 +61,77 @@ const MarketSelectDropdown = () => {
 
           <Popover.Panel className="absolute -left-5 top-[46px] z-50 mr-4 w-screen border border-l-0 border-th-bkg-3 bg-th-bkg-1 sm:w-56 md:top-[37px]">
             {serumMarkets?.length
-              ? serumMarkets.map((m) => (
-                  <div
-                    key={m.publicKey.toString()}
-                    className="bg-th-bkg-1 py-2 px-4 hover:cursor-pointer hover:bg-th-bkg-2"
-                    onClick={() => handleSelectMarket(m, close)}
-                  >
-                    {m.name}
-                  </div>
-                ))
+              ? serumMarkets.map((m) => {
+                  let baseLogoURI = ''
+                  let quoteLogoURI = ''
+                  const tokenSymbols = m.name.split('/')
+                  if (jupiterTokens.length) {
+                    baseLogoURI = jupiterTokens.find(
+                      (t) => t.symbol === tokenSymbols[0]
+                    )!.logoURI
+                    quoteLogoURI = jupiterTokens.find(
+                      (t) => t.symbol === tokenSymbols[1]
+                    )!.logoURI
+                  }
+
+                  return (
+                    <div
+                      key={m.publicKey.toString()}
+                      className="flex items-center bg-th-bkg-1 py-2 px-4 hover:cursor-pointer hover:bg-th-bkg-2"
+                      onClick={() => handleSelectMarket(m, close)}
+                    >
+                      <MarketLogos
+                        baseURI={baseLogoURI}
+                        quoteURI={quoteLogoURI}
+                      />
+                      <span>{m.name}</span>
+                    </div>
+                  )
+                })
               : null}
           </Popover.Panel>
         </div>
       )}
     </Popover>
+  )
+}
+
+const MarketLogos = ({
+  baseURI,
+  quoteURI,
+}: {
+  baseURI: string
+  quoteURI: string
+}) => {
+  return (
+    <div className="relative mr-1.5 h-5 w-[34px]">
+      <div className="absolute left-0 top-0">
+        {baseURI ? (
+          <Image
+            alt=""
+            className="z-10 rounded-full drop-shadow-md"
+            width="20"
+            height="20"
+            src={baseURI}
+          />
+        ) : (
+          <QuestionMarkCircleIcon className="h-5 w-5 text-th-fgd-3" />
+        )}
+      </div>
+      <div className="absolute right-0 top-0">
+        {quoteURI ? (
+          <Image
+            alt=""
+            className="rounded-full opacity-70"
+            width="20"
+            height="20"
+            src={quoteURI}
+          />
+        ) : (
+          <QuestionMarkCircleIcon className="h-5 w-5 text-th-fgd-3" />
+        )}
+      </div>
+    </div>
   )
 }
 
