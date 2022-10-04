a2394d2d284b60307eea76bcca6bc5060d4a1914
tjs <tjshipe@gmail.com>
2022-10-04 13:14:50 -0400

    shrink tooltip hit box

--- components/account/AccountPage.tsx
+++ components/account/AccountPage.tsx
@@ -246,7 +246,7 @@ const AccountPage = () => {
         </div>
       </div>
       <div className="grid grid-cols-4 border-b border-th-bkg-3">
-        <div className="col-span-4 flex border-t border-th-bkg-3 py-3 pl-6 md:col-span-1 md:col-span-2 md:border-l md:border-t-0 lg:col-span-1">
+        <div className="col-span-4 flex border-t border-th-bkg-3 py-3 pl-6 md:col-span-2 md:border-l md:border-t-0 lg:col-span-1">
           <div id="account-step-four">
             <Tooltip
               maxWidth="20rem"
@@ -289,7 +289,7 @@ const AccountPage = () => {
             </p>
           </div>
         </div>
-        <div className="col-span-4 flex border-t border-th-bkg-3 py-3 pl-6 md:col-span-1 md:col-span-2 md:border-l md:border-t-0 lg:col-span-1">
+        <div className="col-span-4 flex border-t border-th-bkg-3 py-3 pl-6 md:col-span-2 md:border-l md:border-t-0 lg:col-span-1">
           <div id="account-step-five">
             <Tooltip
               content="The amount of capital you have to trade or borrow against. When your free collateral reaches $0 you won't be able to make withdrawals."
@@ -313,7 +313,7 @@ const AccountPage = () => {
           </div>
         </div>
         <button
-          className={`col-span-4 border-t border-th-bkg-3 py-3 px-6 md:col-span-2 md:col-span-2 md:border-l lg:col-span-1 lg:border-t-0 ${
+          className={`col-span-4 border-t border-th-bkg-3 py-3 px-6 md:col-span-2 md:border-l lg:col-span-1 lg:border-t-0 ${
             performanceData.length > 4
               ? 'default-transition cursor-pointer md:hover:bg-th-bkg-2'
               : 'cursor-default'
@@ -338,7 +338,7 @@ const AccountPage = () => {
           </div>
         </button>
         <button
-          className={`col-span-4 border-t border-th-bkg-3 py-3 pl-6 text-left md:col-span-1 md:col-span-2 md:border-l lg:col-span-1 lg:border-t-0 ${
+          className={`col-span-4 border-t border-th-bkg-3 py-3 pl-6 text-left md:col-span-2 md:border-l lg:col-span-1 lg:border-t-0 ${
             interestTotalValue > 1 || interestTotalValue < -1
               ? 'default-transition cursor-pointer md:hover:bg-th-bkg-2'
               : 'cursor-default'
