40e7e2b770fbf7162746eb77e732fcdcb6f81e4a
saml33 <slam.uke@gmail.com>
2022-10-26 13:09:26 +1100

    fix bounce loader img src

--- components/shared/BounceLoader.tsx
+++ components/shared/BounceLoader.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const BounceLoader = ({ loadingMessage }: { loadingMessage?: string }) => (
   <div className="flex h-full flex-col items-center justify-center">
     <div className="animate-bounce">
-      <Image src="/icons/mngo.svg" alt="" width="32" height="32" />
+      <Image src="/logos/logo-mark.svg" alt="" width="32" height="32" />
     </div>
     {loadingMessage ? <p className="text-center">{loadingMessage}</p> : null}
   </div>
