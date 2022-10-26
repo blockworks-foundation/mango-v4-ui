11cf4d0542a762e0a5d0befdf83dda522a67b99f
saml33 <slam.uke@gmail.com>
2022-10-26 11:04:09 +1100

    intro tour background

--- styles/colors.ts
+++ styles/colors.ts
@@ -1,5 +1,6 @@
 export const COLORS: any = {
   BKG1: { Mango: '#18181C', Dark: '#17171B', Light: '#FDFDFD' },
+  BKG2: { Mango: '#242132', Dark: '#201F27', Light: '#f0f0f0' },
   GREEN: { Mango: '#A6CD03', Dark: '#5EBF4d', Light: '#5EBF4d' },
   PRIMARY: { Mango: '#F2C94C', Dark: '#F2C94C', Light: '#FF9C24' },
   RED: { Mango: '#F84638', Dark: '#CC2929', Light: '#CC2929' },
