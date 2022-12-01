37ba0a5ec96bd9c9bb01ad3d965411ee6e4b84f1
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-12-01 17:06:19 +0100

    kline charts wip

--- components/trade/TradingViewChart.tsx
+++ components/trade/TradingViewChart.tsx
@@ -1,185 +1,55 @@
-import { useEffect, useRef, useMemo } from 'react'
-import { useTheme } from 'next-themes'
-import {
-  ChartingLibraryWidgetOptions,
-  IChartingLibraryWidget,
-  ResolutionString,
-} from '@public/charting_library'
+import { useEffect, useState } from 'react'
 import mangoStore from '@store/mangoStore'
 import { CHART_DATA_FEED } from 'utils/constants'
-import { COLORS } from 'styles/colors'
 import { init, dispose } from 'klinecharts'
 import axios from 'axios'
 
-export interface ChartContainerProps {
-  container: ChartingLibraryWidgetOptions['container']
-  symbol: ChartingLibraryWidgetOptions['symbol']
-  interval: ChartingLibraryWidgetOptions['interval']
-  datafeedUrl: string
-  libraryPath: ChartingLibraryWidgetOptions['library_path']
-  chartsStorageUrl: ChartingLibraryWidgetOptions['charts_storage_url']
-  chartsStorageApiVersion: ChartingLibraryWidgetOptions['charts_storage_api_version']
-  clientId: ChartingLibraryWidgetOptions['client_id']
-  userId: ChartingLibraryWidgetOptions['user_id']
-  fullscreen: ChartingLibraryWidgetOptions['fullscreen']
-  autosize: ChartingLibraryWidgetOptions['autosize']
-  studiesOverrides: ChartingLibraryWidgetOptions['studies_overrides']
-  theme: string
+const ONE_HOUR_MINS = 60
+const ONE_H = '1H'
+
+const RESOLUTIONS: {
+  [key: string]: string
+} = {
+  '1m': '1',
+  '5m': '5',
+  '30m': `${ONE_HOUR_MINS / 2}`,
+  [ONE_H]: `${ONE_HOUR_MINS}`,
+  '2H': `${2 * ONE_HOUR_MINS}`,
+  '4H': `${4 * ONE_HOUR_MINS}`,
+  '1D': '1D',
+}
+
+type HISTORY = {
+  c: string[]
+  h: string[]
+  l: string[]
+  o: string[]
+  t: number[]
+  v: string[]
 }
 
 const TradingViewChart = () => {
-  const { theme } = useTheme()
-
+  //const { theme } = useTheme()
   const selectedMarketName = mangoStore((s) => s.selectedMarket.current?.name)
-  const interval = 60
-  console.log(selectedMarketName)
+  const [resolution, setResultion] = useState(RESOLUTIONS['1H'])
+  const [chart, setChart] = useState<klinecharts.Chart | null>(null)
   const query = {
-    resolution: interval,
+    resolution: resolution,
     symbol: 'SOL/USDC',
-    from: Math.floor(Date.now() / 1000) - 60 * 60 * 24 * 14,
+    from: Math.floor(Date.now() / 1000) - 60 * 60 * 24 * 30,
     to: Math.floor(Date.now() / 1000),
-    countback: 2,
   }
-  const defaultProps = useMemo(
-    () => ({
-      symbol: 'SOL/USDC',
-      interval: '60' as ResolutionString,
-      theme: 'Dark',
-      container: 'tv_chart_container',
-      datafeedUrl: CHART_DATA_FEED,
-      libraryPath: '/charting_library/',
-      fullscreen: false,
-      autosize: true,
-      studiesOverrides: {
-        'volume.volume.color.0': COLORS.RED[theme],
-        'volume.volume.color.1': COLORS.GREEN[theme],
-        'volume.precision': 4,
-      },
-    }),
-    [theme]
-  )
-  console.log(defaultProps)
-  const tvWidgetRef = useRef<IChartingLibraryWidget | null>(null)
-
-  let chartStyleOverrides = {
-    'paneProperties.background': 'rgba(0,0,0,0)',
-    'paneProperties.backgroundType': 'solid',
-    'paneProperties.legendProperties.showBackground': false,
-    'paneProperties.vertGridProperties.color': 'rgba(0,0,0,0)',
-    'paneProperties.horzGridProperties.color': 'rgba(0,0,0,0)',
-    'paneProperties.legendProperties.showStudyTitles': false,
-    'scalesProperties.showStudyLastValue': false,
-    'scalesProperties.fontSize': 11,
-  }
-
-  const mainSeriesProperties = [
-    'candleStyle',
-    'hollowCandleStyle',
-    'haStyle',
-    'barStyle',
-  ]
-
-  mainSeriesProperties.forEach((prop) => {
-    chartStyleOverrides = {
-      ...chartStyleOverrides,
-      [`mainSeriesProperties.${prop}.barColorsOnPrevClose`]: true,
-      [`mainSeriesProperties.${prop}.drawWick`]: true,
-      [`mainSeriesProperties.${prop}.drawBorder`]: true,
-      [`mainSeriesProperties.${prop}.upColor`]: COLORS.GREEN[theme],
-      [`mainSeriesProperties.${prop}.downColor`]: COLORS.RED[theme],
-      [`mainSeriesProperties.${prop}.borderColor`]: COLORS.GREEN[theme],
-      [`mainSeriesProperties.${prop}.borderUpColor`]: COLORS.GREEN[theme],
-      [`mainSeriesProperties.${prop}.borderDownColor`]: COLORS.RED[theme],
-      [`mainSeriesProperties.${prop}.wickUpColor`]: COLORS.GREEN[theme],
-      [`mainSeriesProperties.${prop}.wickDownColor`]: COLORS.RED[theme],
-    }
-  })
 
   useEffect(() => {
-    if (tvWidgetRef.current && selectedMarketName) {
-      tvWidgetRef.current.setSymbol(
-        selectedMarketName!,
-        tvWidgetRef.current.activeChart().resolution(),
-        () => {
-          return
-        }
-      )
-    }
+    return
   }, [selectedMarketName])
 
-  //   useEffect(() => {
-  //     if (window) {
-  //       const widgetOptions: ChartingLibraryWidgetOptions = {
-  //         // debug: true,
-  //         symbol: defaultProps.symbol,
-  //         // BEWARE: no trailing slash is expected in feed URL
-  //         // tslint:disable-next-line:no-any
-  //         datafeed: new (window as any).Datafeeds.UDFCompatibleDatafeed(
-  //           defaultProps.datafeedUrl
-  //         ),
-  //         interval:
-  //           defaultProps.interval as ChartingLibraryWidgetOptions['interval'],
-  //         container:
-  //           defaultProps.container as ChartingLibraryWidgetOptions['container'],
-  //         library_path: defaultProps.libraryPath as string,
-  //         locale: 'en',
-  //         enabled_features: ['hide_left_toolbar_by_default'],
-  //         disabled_features: [
-  //           'use_localstorage_for_settings',
-  //           'timeframes_toolbar',
-  //           isMobile ? 'left_toolbar' : '',
-  //           'show_logo_on_all_charts',
-  //           'caption_buttons_text_if_possible',
-  //           'header_settings',
-  //           // 'header_chart_type',
-  //           'header_compare',
-  //           'compare_symbol',
-  //           'header_screenshot',
-  //           // 'header_widget_dom_node',
-  //           // 'header_widget',
-  //           'header_saveload',
-  //           'header_undo_redo',
-  //           'header_interval_dialog_button',
-  //           'show_interval_dialog_on_key_press',
-  //           'header_symbol_search',
-  //           'popup_hints',
-  //         ],
-  //         fullscreen: defaultProps.fullscreen,
-  //         autosize: defaultProps.autosize,
-  //         studies_overrides: defaultProps.studiesOverrides,
-  //         theme: theme === 'Light' ? 'Light' : 'Dark',
-  //         custom_css_url: '/styles/tradingview.css',
-  //         loading_screen: {
-  //           backgroundColor:
-  //             theme === 'Dark'
-  //               ? COLORS.BKG1.Dark
-  //               : theme === 'Light'
-  //               ? COLORS.BKG1.Light
-  //               : COLORS.BKG1.Mango,
-  //         },
-  //         overrides: {
-  //           timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-
-  //           ...chartStyleOverrides,
-  //         },
-  //       }
-
-  //       const tvWidget = new widget(widgetOptions)
-  //       tvWidgetRef.current = tvWidget
-
-  //       tvWidgetRef.current.onChartReady(function () {
-  //         setChartReady(true)
-  //       })
-  //       //eslint-disable-next-line
-  //     }
-  //   }, [theme, isMobile, defaultProps])
-
-  const generatedKLineDataList = async (dataSize = 336) => {
+  const fetchData = async () => {
     const response = await axios.get(`${CHART_DATA_FEED}/history`, {
       params: query,
     })
-    const newData = response.data as any
-
+    const newData = response.data as HISTORY
+    const dataSize = newData.t.length
     const dataList = []
     for (let i = 0; i < dataSize; i++) {
       const kLineModel = {
@@ -188,9 +58,9 @@ const TradingViewChart = () => {
         high: parseFloat(newData.h[i]),
         close: parseFloat(newData.c[i]),
         volume: parseFloat(newData.v[i]),
-        timestamp: newData.t[i],
+        timestamp: newData.t[i] * 1000,
       }
-      dataList.unshift(kLineModel)
+      dataList.push(kLineModel)
     }
     return dataList
   }
@@ -198,19 +68,49 @@ const TradingViewChart = () => {
   function updateData(kLineChart: any) {
     setTimeout(async () => {
       if (kLineChart) {
-        const newData = (await generatedKLineDataList(1))[0]
-        newData.timestamp += 30000 * 60
+        const newData = (await fetchData())[0]
+        newData.timestamp += 60000
         kLineChart.updateData(newData)
       }
       updateData(kLineChart)
-    }, 30000)
+    }, 60000)
   }
+  useEffect(() => {
+    const changeResultion = async () => {
+      const data = await fetchData()
+      chart!.applyNewData(data)
+      updateData(chart)
+    }
+    if (chart) {
+      changeResultion()
+    }
+  }, [resolution])
 
   useEffect(() => {
     const initKline = async () => {
+      const style = getComputedStyle(document.body)
+      const gridColor = style.getPropertyValue('--bkg-3')
       const kLineChart = init('update-k-line')
-      const data = await generatedKLineDataList()
+
+      kLineChart.setStyleOptions({
+        grid: {
+          show: true,
+          horizontal: {
+            color: gridColor,
+          },
+          vertical: {
+            color: gridColor,
+          },
+        },
+        candle: {
+          tooltip: {
+            labels: ['T: ', 'O: ', 'C: ', 'H: ', 'L: ', 'V: '],
+          },
+        },
+      })
+      const data = await fetchData()
       kLineChart.applyNewData(data)
+      setChart(kLineChart)
       updateData(kLineChart)
     }
     initKline()
@@ -220,8 +120,19 @@ const TradingViewChart = () => {
   }, [])
   return (
     <>
+      <div className="flex">
+        {Object.keys(RESOLUTIONS).map((key) => (
+          <div
+            className="cursor-pointer py-1 px-2"
+            key={key}
+            onClick={() => setResultion(RESOLUTIONS[key])}
+          >
+            {key}
+          </div>
+        ))}
+      </div>
       <div
-        style={{ height: '300px' }}
+        style={{ height: 'calc(100% - 30px)' }}
         id="update-k-line"
         className="k-line-chart"
       />
