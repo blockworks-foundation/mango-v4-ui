c5c88ce3c2ead32063217837b3209c77737c6085
saml33 <slam.uke@gmail.com>
2022-12-07 16:25:37 +1100

    trade header info on mobile

--- components/trade/AdvancedMarketHeader.tsx
+++ components/trade/AdvancedMarketHeader.tsx
@@ -33,32 +33,34 @@ const AdvancedMarketHeader = () => {
     : 0
 
   return (
-    <div className="flex h-16 items-center bg-th-bkg-1 px-5 md:h-12">
-      <div className="md:pr-6 lg:pb-0">
-        <div className="flex items-center">
-          <MarketSelectDropdown />
+    <div className="flex flex-col bg-th-bkg-1 md:h-12 md:flex-row md:items-center">
+      <div className=" w-full px-5 md:w-auto md:py-0 md:pr-6 lg:pb-0">
+        <MarketSelectDropdown />
+      </div>
+      <div className="flex items-center border-t border-th-bkg-3 py-2 px-5 md:border-t-0 md:py-0 md:px-0">
+        <div id="trade-step-two" className="flex-col md:ml-6">
+          <div className="text-xs text-th-fgd-4">{t('trade:oracle-price')}</div>
+          <div className="font-mono text-xs text-th-fgd-2">
+            {oraclePrice ? (
+              `$${formatFixedDecimals(oraclePrice)}`
+            ) : (
+              <span className="text-th-fgd-4">–</span>
+            )}
+          </div>
         </div>
-      </div>
-      <div id="trade-step-two" className="ml-6 flex-col">
-        <div className="text-xs text-th-fgd-4">{t('trade:oracle-price')}</div>
-        <div className="font-mono text-xs text-th-fgd-2">
-          {oraclePrice ? (
-            `$${formatFixedDecimals(oraclePrice)}`
-          ) : (
-            <span className="text-th-fgd-4">–</span>
-          )}
-        </div>
-      </div>
-      <div className="ml-6 flex-col">
-        <div className="text-xs text-th-fgd-4">{t('rolling-change')}</div>
-        <Change change={change} size="small" suffix="%" />
-      </div>
-      {selectedMarket instanceof PerpMarket ? (
         <div className="ml-6 flex-col">
-          <div className="text-xs text-th-fgd-4">{t('trade:funding-rate')}</div>
-          <PerpFundingRate />
+          <div className="text-xs text-th-fgd-4">{t('rolling-change')}</div>
+          <Change change={change} size="small" suffix="%" />
         </div>
-      ) : null}
+        {selectedMarket instanceof PerpMarket ? (
+          <div className="ml-6 flex-col">
+            <div className="text-xs text-th-fgd-4">
+              {t('trade:funding-rate')}
+            </div>
+            <PerpFundingRate />
+          </div>
+        ) : null}
+      </div>
     </div>
   )
 }
