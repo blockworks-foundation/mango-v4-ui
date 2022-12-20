c6a8806c26415b3caaa4784e48a47765c2564442
tjs <tjshipe@gmail.com>
2022-12-20 13:05:53 -0500

    hide perps correctly

--- components/stats/StatsPage.tsx
+++ components/stats/StatsPage.tsx
@@ -4,9 +4,10 @@ import PerpMarketsTable from './PerpMarketsTable'
 import SpotMarketsTable from './SpotMarketsTable'
 import TokenStats from './TokenStats'
 
-const TABS = process.env.NEXT_PUBLIC_SHOW_PERPS
-  ? ['tokens', 'perp', 'spot']
-  : ['tokens', 'spot']
+const TABS =
+  process.env.NEXT_PUBLIC_SHOW_PERPS === 'true'
+    ? ['tokens', 'perp', 'spot']
+    : ['tokens', 'spot']
 
 const StatsPage = () => {
   const [activeTab, setActiveTab] = useState('tokens')
