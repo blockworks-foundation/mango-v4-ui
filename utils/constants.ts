70da12776b8d22e2a7f369fbc38e567e7dd8419d
tjs <tjshipe@gmail.com>
2022-12-19 19:24:46 -0500

    update local storage key names

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
-export const LAST_ACCOUNT_KEY = 'mangoAccount-0.1'
+export const LAST_ACCOUNT_KEY = 'mangoAccount-0.2'
 
 export const CLIENT_TX_TIMEOUT = 90000
 
@@ -10,24 +10,32 @@ export const OUTPUT_TOKEN_DEFAULT = 'SOL'
 export const JUPITER_V4_PROGRAM_ID =
   'JUP4Fb2cqiRUcaTHdrPC8h2gNsA2ETXiPDD33WcGuJB'
 
+export const ALPHA_DEPOSIT_LIMIT = 20
+
+// Local storage keys for settings
 export const IS_ONBOARDED_KEY = 'isOnboarded-0.1'
 
-export const SHOW_ZERO_BALANCES_KEY = 'show-zero-balances-0.1'
-
-export const ALPHA_DEPOSIT_LIMIT = 20
+export const SHOW_ZERO_BALANCES_KEY = 'show-zero-balances-0.2'
 
 export const SIDEBAR_COLLAPSE_KEY = 'sidebar-0.1'
 
-export const ONBOARDING_TOUR_KEY = 'showOnboardingTour'
+export const ONBOARDING_TOUR_KEY = 'showOnboardingTour-0.1'
 
-export const PREFERRED_EXPLORER_KEY = 'preferredExplorer'
+export const PREFERRED_EXPLORER_KEY = 'preferredExplorer-0.1'
 
-export const ANIMATION_SETTINGS_KEY = 'animationSettings'
+export const ANIMATION_SETTINGS_KEY = 'animationSettings-0.1'
 
-export const SOUND_SETTINGS_KEY = 'soundSettings'
+export const SOUND_SETTINGS_KEY = 'soundSettings-0.1'
 
-export const SIZE_INPUT_UI_KEY = 'tradeFormUi'
+export const SIZE_INPUT_UI_KEY = 'tradeFormUi-0.1'
 
+export const GRID_LAYOUT_KEY = 'savedLayouts-0.2'
+
+export const NOTIFICATION_POSITION_KEY = 'notificationPosition-0.1'
+
+export const FAVORITE_MARKETS_KEY = 'favoriteMarkets-0.2'
+
+// Unused
 export const PROFILE_CATEGORIES = [
   'borrower',
   'day-trader',
@@ -44,13 +52,8 @@ export const CHART_DATA_FEED = `https://dry-ravine-67635.herokuapp.com/tv`
 
 export const DEFAULT_MARKET_NAME = 'SOL/USDC'
 
-export const GRID_LAYOUT_KEY = 'savedLayouts-0.1'
-
-export const NOTIFICATION_POSITION_KEY = 'notificationPosition'
-
-export const FAVORITE_MARKETS_KEY = 'favoriteMarkets'
-
 export const MIN_SOL_BALANCE = 0.04
 
 export const ACCOUNT_ACTION_MODAL_HEIGHT = '448px'
+
 export const ACCOUNT_ACTION_MODAL_INNER_HEIGHT = '386px'
