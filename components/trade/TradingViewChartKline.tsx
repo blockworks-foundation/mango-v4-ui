359ddc19b4ec2cfb5f1f5c1583d1bfe9bcbeea70
saml33 <slam.uke@gmail.com>
2022-12-26 21:13:07 +1100

    style kline chart

--- components/trade/TradingViewChartKline.tsx
+++ components/trade/TradingViewChartKline.tsx
@@ -9,8 +9,8 @@ import Switch from '@components/forms/Switch'
 import {
   BASE_CHART_QUERY,
   CHART_QUERY,
-  DEFAULT_MAIN_INDICATORS,
-  DEFAULT_SUB_INDICATOR,
+  // DEFAULT_MAIN_INDICATORS,
+  // DEFAULT_SUB_INDICATOR,
   HISTORY,
   mainTechnicalIndicatorTypes,
   MAIN_INDICATOR_CLASS,
@@ -18,10 +18,13 @@ import {
   RES_NAME_TO_RES_VAL,
   subTechnicalIndicatorTypes,
 } from 'utils/kLineChart'
-import { ArrowsPointingOutIcon } from '@heroicons/react/24/outline'
-import Loading from '@components/shared/Loading'
+// import Loading from '@components/shared/Loading'
 import clsx from 'clsx'
 import { API_URL, BE_API_KEY } from 'apis/birdeye/helpers'
+import { useTheme } from 'next-themes'
+import { COLORS } from 'styles/colors'
+import { IconButton } from '@components/shared/Button'
+import { ArrowsPointingOutIcon, XMarkIcon } from '@heroicons/react/20/solid'
 
 const UPDATE_INTERVAL = 10000
 
@@ -32,6 +35,7 @@ type Props = {
 
 const TradingViewChartKline = ({ setIsFullView, isFullView }: Props) => {
   const { width } = useViewport()
+  const { theme } = useTheme()
   const prevWidth = usePrevious(width)
   const selectedMarket = mangoStore((s) => s.selectedMarket.current)
   const selectedMarketName = selectedMarket?.name
@@ -43,15 +47,15 @@ const TradingViewChartKline = ({ setIsFullView, isFullView }: Props) => {
     //indicatorName: class
     [indicatorName: string]: string
   }>({})
-  const [isLoading, setIsLoading] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
   const [resolution, setResolution] = useState(RES_NAME_TO_RES_VAL['1H'])
   const [chart, setChart] = useState<klinecharts.Chart | null>(null)
-  const previousChart = usePrevious(chart)
+  // const previousChart = usePrevious(chart)
   const [baseChartQuery, setQuery] = useState<BASE_CHART_QUERY | null>(null)
   const clearTimerRef = useRef<NodeJS.Timeout | null>(null)
   const fetchData = async (baseQuery: BASE_CHART_QUERY, from: number) => {
     try {
-      setIsLoading(true)
+      // setIsLoading(true)
       const query: CHART_QUERY = {
         ...baseQuery,
         time_from: from,
@@ -77,10 +81,10 @@ const TradingViewChartKline = ({ setIsFullView, isFullView }: Props) => {
         }
         dataList.push(kLineModel)
       }
-      setIsLoading(false)
+      // setIsLoading(false)
       return dataList
     } catch (e) {
-      setIsLoading(false)
+      // setIsLoading(false)
       console.log(e)
       return []
     }
@@ -155,67 +159,248 @@ const TradingViewChartKline = ({ setIsFullView, isFullView }: Props) => {
     }
   }, [selectedMarketName, resolution])
 
-  //init default technical indicators after init of chart
-  useEffect(() => {
-    if (chart !== null && previousChart === null) {
-      if (DEFAULT_SUB_INDICATOR) {
-        const subId = chart.createTechnicalIndicator(
-          DEFAULT_SUB_INDICATOR,
-          true
-        )
-        setSubTechnicalIndicators({ [DEFAULT_SUB_INDICATOR]: subId })
-      }
-      if (DEFAULT_MAIN_INDICATORS?.length) {
-        for (const type of DEFAULT_MAIN_INDICATORS) {
-          chart?.createTechnicalIndicator(type, true, {
-            id: MAIN_INDICATOR_CLASS,
-          })
-        }
-        setMainTechnicalIndicators(DEFAULT_MAIN_INDICATORS)
-      }
-    }
-  }, [chart !== null])
+  // init default technical indicators after init of chart
+  // useEffect(() => {
+  //   if (chart !== null && previousChart === null) {
+  //     if (DEFAULT_SUB_INDICATOR) {
+  //       const subId = chart.createTechnicalIndicator(
+  //         DEFAULT_SUB_INDICATOR,
+  //         true
+  //       )
+  //       setSubTechnicalIndicators({ [DEFAULT_SUB_INDICATOR]: subId })
+  //     }
+  //     if (DEFAULT_MAIN_INDICATORS?.length) {
+  //       for (const type of DEFAULT_MAIN_INDICATORS) {
+  //         chart?.createTechnicalIndicator(type, true, {
+  //           id: MAIN_INDICATOR_CLASS,
+  //         })
+  //       }
+  //       setMainTechnicalIndicators(DEFAULT_MAIN_INDICATORS)
+  //     }
+  //   }
+  // }, [chart !== null])
 
   //init chart without data
   useEffect(() => {
     const initKline = async () => {
-      const style = getComputedStyle(document.body)
-      const gridColor = style.getPropertyValue('--bkg-3')
       const kLineChart = init('update-k-line')
       kLineChart.setStyleOptions({
         grid: {
-          show: true,
-          horizontal: {
-            style: 'solid',
-            color: gridColor,
-          },
-          vertical: {
-            style: 'solid',
-            color: gridColor,
-          },
+          show: false,
         },
         candle: {
+          bar: {
+            upColor: COLORS.UP[theme],
+            downColor: COLORS.DOWN[theme],
+          },
           tooltip: {
-            labels: ['T: ', 'O: ', 'C: ', 'H: ', 'L: ', 'V: '],
+            labels: ['', 'O:', 'C:', 'H:', 'L:', 'V:'],
+            text: {
+              size: 12,
+              family: 'TT Mono',
+              weight: 'normal',
+              color: COLORS.FGD4[theme],
+              marginLeft: 8,
+              marginTop: 6,
+              marginRight: 8,
+              marginBottom: 0,
+            },
+          },
+          priceMark: {
+            show: true,
+            high: {
+              show: true,
+              color: COLORS.FGD4[theme],
+              textMargin: 5,
+              textSize: 10,
+              textFamily: 'TT Mono',
+              textWeight: 'normal',
+            },
+            low: {
+              show: true,
+              color: COLORS.FGD4[theme],
+              textMargin: 5,
+              textSize: 10,
+              textFamily: 'TT Mono',
+              textWeight: 'normal',
+            },
+            last: {
+              show: true,
+              upColor: COLORS.BKG4[theme],
+              downColor: COLORS.BKG4[theme],
+              noChangeColor: COLORS.BKG4[theme],
+              line: {
+                show: true,
+                // 'solid'|'dash'
+                style: 'dash',
+                dashValue: [4, 4],
+                size: 1,
+              },
+              text: {
+                show: true,
+                size: 10,
+                paddingLeft: 2,
+                paddingTop: 2,
+                paddingRight: 2,
+                paddingBottom: 2,
+                color: '#FFFFFF',
+                family: 'TT Mono',
+                weight: 'normal',
+                borderRadius: 2,
+              },
+            },
           },
         },
         xAxis: {
           axisLine: {
             show: true,
-            color: gridColor,
+            color: COLORS.BKG4[theme],
             size: 1,
+          },
+          tickLine: {
+            show: true,
+            size: 1,
+            length: 3,
+            color: COLORS.BKG4[theme],
+          },
+          tickText: {
+            show: true,
+            color: COLORS.FGD4[theme],
+            family: 'TT Mono',
+            weight: 'normal',
+            size: 10,
           },
         },
         yAxis: {
           axisLine: {
             show: true,
-            color: gridColor,
+            color: COLORS.BKG4[theme],
             size: 1,
+          },
+          tickLine: {
+            show: true,
+            size: 1,
+            length: 3,
+            color: COLORS.BKG4[theme],
+          },
+          tickText: {
+            show: true,
+            color: COLORS.FGD4[theme],
+            family: 'TT Mono',
+            weight: 'normal',
+            size: 10,
+          },
+        },
+        crosshair: {
+          show: true,
+          horizontal: {
+            show: true,
+            line: {
+              show: true,
+              style: 'dash',
+              dashValue: [4, 2],
+              size: 1,
+              color: COLORS.FGD4[theme],
+            },
+            text: {
+              show: true,
+              color: '#FFFFFF',
+              size: 10,
+              family: 'TT Mono',
+              weight: 'normal',
+              paddingLeft: 2,
+              paddingRight: 2,
+              paddingTop: 2,
+              paddingBottom: 2,
+              borderSize: 1,
+              borderColor: COLORS.FGD4[theme],
+              borderRadius: 2,
+              backgroundColor: COLORS.FGD4[theme],
+            },
+          },
+          vertical: {
+            show: true,
+            line: {
+              show: true,
+              style: 'dash',
+              dashValue: [4, 2],
+              size: 1,
+              color: COLORS.FGD4[theme],
+            },
+            text: {
+              show: true,
+              color: '#FFFFFF',
+              size: 10,
+              family: 'TT Mono',
+              weight: 'normal',
+              paddingLeft: 2,
+              paddingRight: 2,
+              paddingTop: 2,
+              paddingBottom: 2,
+              borderSize: 1,
+              borderColor: COLORS.FGD4[theme],
+              borderRadius: 2,
+              backgroundColor: COLORS.FGD4[theme],
+            },
+          },
+        },
+        technicalIndicator: {
+          margin: {
+            top: 0.2,
+            bottom: 0.1,
+          },
+          bar: {
+            upColor: COLORS.UP[theme],
+            downColor: COLORS.DOWN[theme],
+            noChangeColor: '#888888',
+          },
+          line: {
+            size: 1,
+            colors: ['#FF9600', '#9D65C9', '#2196F3', '#E11D74', '#01C5C4'],
+          },
+          circle: {
+            upColor: '#26A69A',
+            downColor: '#EF5350',
+            noChangeColor: '#888888',
+          },
+          lastValueMark: {
+            show: false,
+            text: {
+              show: false,
+              color: '#ffffff',
+              size: 12,
+              family: 'Helvetica Neue',
+              weight: 'normal',
+              paddingLeft: 3,
+              paddingTop: 2,
+              paddingRight: 3,
+              paddingBottom: 2,
+              borderRadius: 2,
+            },
+          },
+          tooltip: {
+            // 'always' | 'follow_cross' | 'none'
+            showRule: 'always',
+            // 'standard' | 'rect'
+            showType: 'standard',
+            showName: true,
+            showParams: true,
+            defaultValue: 'n/a',
+            text: {
+              size: 12,
+              family: 'TT Mono',
+              weight: 'normal',
+              color: COLORS.FGD4[theme],
+              marginTop: 6,
+              marginRight: 8,
+              marginBottom: 0,
+              marginLeft: 8,
+            },
           },
         },
         separator: {
           size: 2,
-          color: gridColor,
+          color: COLORS.BKG4[theme],
         },
       })
       setChart(kLineChart)
@@ -230,43 +415,52 @@ const TradingViewChartKline = ({ setIsFullView, isFullView }: Props) => {
     <div
       className={clsx(
         'fixed h-full w-full',
-        isFullView &&
-          'left-[64px] top-0 right-0 bottom-0 bg-th-bkg-1 text-th-fgd-1'
+        isFullView
+          ? 'left-0 top-0 right-0 bottom-0 z-40 bg-th-bkg-1 text-th-fgd-1'
+          : ''
       )}
     >
-      <div className="flex w-full">
-        {Object.keys(RES_NAME_TO_RES_VAL).map((key) => (
-          <div
-            className={clsx(
-              'cursor-pointer py-1 px-2',
-              resolution === RES_NAME_TO_RES_VAL[key] && 'text-th-active'
-            )}
-            key={key}
-            onClick={() => setResolution(RES_NAME_TO_RES_VAL[key])}
+      <div className="mb-1 flex w-full items-center justify-between border-b border-th-bkg-3 p-1 text-th-fgd-2">
+        <div className="flex text-th-fgd-3">
+          {Object.keys(RES_NAME_TO_RES_VAL).map((key) => (
+            <button
+              className={clsx(
+                'default-transition px-2 md:hover:text-th-fgd-2',
+                resolution === RES_NAME_TO_RES_VAL[key] && 'text-th-active'
+              )}
+              key={key}
+              onClick={() => setResolution(RES_NAME_TO_RES_VAL[key])}
+            >
+              {key}
+            </button>
+          ))}
+          <button
+            className="default-transition px-2 md:hover:text-th-fgd-2"
+            onClick={() => setIsTechnicalModalOpen(true)}
           >
-            {key}
-          </div>
-        ))}
-        <div
-          className="cursor-pointer py-1 px-2 "
-          onClick={() => setIsTechnicalModalOpen(true)}
-        >
-          Indicator
+            Indicator
+          </button>
         </div>
-        {setIsFullView && (
-          <div className="cursor-pointer py-1 px-2">
-            <ArrowsPointingOutIcon
-              onClick={() => setIsFullView(!isFullView)}
-              className="w-5"
-            ></ArrowsPointingOutIcon>
-          </div>
-        )}
-        <div className="py-1 px-2">
+        {setIsFullView ? (
+          <IconButton
+            className="text-th-fgd-3"
+            size="small"
+            hideBg
+            onClick={() => setIsFullView(!isFullView)}
+          >
+            {isFullView ? (
+              <XMarkIcon className="h-5 w-5" />
+            ) : (
+              <ArrowsPointingOutIcon className="h-5 w-5" />
+            )}
+          </IconButton>
+        ) : null}
+        {/* <div className="px-2">
           {isLoading && <Loading className="w-4"></Loading>}
-        </div>
+        </div> */}
       </div>
       <div
-        style={{ height: 'calc(100% - 30px)', width: '100%' }}
+        style={{ height: 'calc(100% - 48px)', width: '100%' }}
         id="update-k-line"
         className="k-line-chart"
       />
@@ -275,7 +469,7 @@ const TradingViewChartKline = ({ setIsFullView, isFullView }: Props) => {
         onClose={() => setIsTechnicalModalOpen(false)}
       >
         <div className="hide-scroll flex max-h-96 flex-col overflow-auto text-left">
-          <h2 className="py-4">Main Indicator</h2>
+          <h2 className="py-4 text-base">Main Indicator</h2>
           {mainTechnicalIndicatorTypes.map((type) => {
             return (
               <IndicatorSwitch
@@ -301,7 +495,7 @@ const TradingViewChartKline = ({ setIsFullView, isFullView }: Props) => {
               ></IndicatorSwitch>
             )
           })}
-          <h2 className="py-4">Sub Indicator</h2>
+          <h2 className="py-4 text-base">Bottom Indicator</h2>
           {subTechnicalIndicatorTypes.map((type) => {
             return (
               <IndicatorSwitch
