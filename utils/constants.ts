4c7ade34920b50af715e23171d753a16a551ec5a
tjs <tjshipe@gmail.com>
2023-01-02 16:48:31 -0500

    increase alpha limit

--- utils/constants.ts
+++ utils/constants.ts
@@ -10,7 +10,7 @@ export const OUTPUT_TOKEN_DEFAULT = 'SOL'
 export const JUPITER_V4_PROGRAM_ID =
   'JUP4Fb2cqiRUcaTHdrPC8h2gNsA2ETXiPDD33WcGuJB'
 
-export const ALPHA_DEPOSIT_LIMIT = 20
+export const ALPHA_DEPOSIT_LIMIT = 100
 
 // Local storage keys for settings
 export const IS_ONBOARDED_KEY = 'isOnboarded-0.1'
