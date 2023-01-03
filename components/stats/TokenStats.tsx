d4fc0ceb86a877dcd036c1f9c483e9c0d47ee129
saml33 <slam.uke@gmail.com>
2023-01-03 19:33:30 +1100

    sort token stats table mobile

--- components/stats/TokenStats.tsx
+++ components/stats/TokenStats.tsx
@@ -197,124 +197,129 @@ const TokenStats = () => {
         </Table>
       ) : (
         <div>
-          {banks.map(({ key, value }) => {
-            const bank = value[0]
-            let logoURI
-            if (mangoTokens?.length) {
-              logoURI = mangoTokens.find(
-                (t) => t.address === bank.mint.toString()
-              )?.logoURI
-            }
-            return (
-              <div key={key} className="border-b border-th-bkg-3 px-6 py-4">
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center">
-                    <div className="mr-2.5 flex flex-shrink-0 items-center">
-                      {logoURI ? (
-                        <Image alt="" width="24" height="24" src={logoURI} />
-                      ) : (
-                        <QuestionMarkCircleIcon className="h-7 w-7 text-th-fgd-3" />
-                      )}
+          {banks
+            .sort((a, b) => a.key.localeCompare(b.key))
+            .map(({ key, value }) => {
+              const bank = value[0]
+              let logoURI
+              if (mangoTokens?.length) {
+                logoURI = mangoTokens.find(
+                  (t) => t.address === bank.mint.toString()
+                )?.logoURI
+              }
+              return (
+                <div key={key} className="border-b border-th-bkg-3 px-6 py-4">
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                      <div className="mr-2.5 flex flex-shrink-0 items-center">
+                        {logoURI ? (
+                          <Image alt="" width="24" height="24" src={logoURI} />
+                        ) : (
+                          <QuestionMarkCircleIcon className="h-7 w-7 text-th-fgd-3" />
+                        )}
+                      </div>
+                      <p className="text-th-fgd-1">{bank.name}</p>
                     </div>
-                    <p className="text-th-fgd-1">{bank.name}</p>
-                  </div>
-                  <div className="flex items-center space-x-4">
-                    <div>
-                      <p className="text-right text-xs">
-                        {t('total-deposits')}
-                      </p>
-                      <p className="text-right font-mono text-th-fgd-1">
-                        {formatFixedDecimals(bank.uiDeposits())}
-                      </p>
-                    </div>
-                    <div>
-                      <p className="text-right text-xs">{t('total-borrows')}</p>
-                      <p className="text-right font-mono text-th-fgd-1">
-                        {formatFixedDecimals(bank.uiBorrows())}
-                      </p>
-                    </div>
-                    <IconButton
-                      onClick={() => handleShowTokenDetails(bank.name)}
-                    >
-                      <ChevronDownIcon
-                        className={`${
-                          showTokenDetails === bank.name
-                            ? 'rotate-180'
-                            : 'rotate-360'
-                        } h-6 w-6 flex-shrink-0 text-th-fgd-1`}
-                      />
-                    </IconButton>
-                  </div>
-                </div>
-                <Transition
-                  appear={true}
-                  show={showTokenDetails === bank.name}
-                  as={Fragment}
-                  enter="transition ease-in duration-200"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="transition ease-out"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="mt-4 grid grid-cols-2 gap-4 border-t border-th-bkg-3 pt-4">
-                    <div className="col-span-1">
-                      <p className="text-xs text-th-fgd-3">{t('rates')}</p>
-                      <p className="space-x-2">
-                        <span className="font-mono text-th-up">
-                          {formatDecimal(bank.getDepositRate().toNumber(), 2)}%
-                        </span>
-                        <span className="font-normal text-th-fgd-4">|</span>
-                        <span className="font-mono text-th-down">
-                          {formatDecimal(bank.getBorrowRate().toNumber(), 2)}%
-                        </span>
-                      </p>
-                    </div>
-                    <div className="col-span-1">
-                      <p className="text-xs text-th-fgd-3">
-                        {t('utilization')}
-                      </p>
-                      <p className="font-mono text-th-fgd-1">
-                        {bank.uiDeposits() > 0
-                          ? formatDecimal(
-                              (bank.uiBorrows() / bank.uiDeposits()) * 100,
-                              1,
-                              { fixed: true }
-                            )
-                          : '0.0'}
-                        %
-                      </p>
-                    </div>
-                    <div className="col-span-1">
-                      <p className="text-xs text-th-fgd-3">
-                        {t('asset-weight')}
-                      </p>
-                      <p className="font-mono text-th-fgd-1">
-                        {bank.initAssetWeight.toFixed(2)}
-                      </p>
-                    </div>
-                    <div className="col-span-1">
-                      <p className="text-xs text-th-fgd-3">
-                        {t('liability-weight')}
-                      </p>
-                      <p className="font-mono text-th-fgd-1">
-                        {bank.initLiabWeight.toFixed(2)}
-                      </p>
-                    </div>
-                    <div className="col-span-1">
-                      <LinkButton
-                        className="flex items-center"
-                        onClick={() => goToTokenPage(bank)}
+                    <div className="flex items-center space-x-4">
+                      <div>
+                        <p className="text-right text-xs">
+                          {t('total-deposits')}
+                        </p>
+                        <p className="text-right font-mono text-th-fgd-1">
+                          {formatFixedDecimals(bank.uiDeposits())}
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-right text-xs">
+                          {t('total-borrows')}
+                        </p>
+                        <p className="text-right font-mono text-th-fgd-1">
+                          {formatFixedDecimals(bank.uiBorrows())}
+                        </p>
+                      </div>
+                      <IconButton
+                        onClick={() => handleShowTokenDetails(bank.name)}
                       >
-                        {t('token:token-details')}
-                        <ChevronRightIcon className="ml-2 h-5 w-5" />
-                      </LinkButton>
+                        <ChevronDownIcon
+                          className={`${
+                            showTokenDetails === bank.name
+                              ? 'rotate-180'
+                              : 'rotate-360'
+                          } h-6 w-6 flex-shrink-0 text-th-fgd-1`}
+                        />
+                      </IconButton>
                     </div>
                   </div>
-                </Transition>
-              </div>
-            )
-          })}
+                  <Transition
+                    appear={true}
+                    show={showTokenDetails === bank.name}
+                    as={Fragment}
+                    enter="transition ease-in duration-200"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="transition ease-out"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="mt-4 grid grid-cols-2 gap-4 border-t border-th-bkg-3 pt-4">
+                      <div className="col-span-1">
+                        <p className="text-xs text-th-fgd-3">{t('rates')}</p>
+                        <p className="space-x-2">
+                          <span className="font-mono text-th-up">
+                            {formatDecimal(bank.getDepositRate().toNumber(), 2)}
+                            %
+                          </span>
+                          <span className="font-normal text-th-fgd-4">|</span>
+                          <span className="font-mono text-th-down">
+                            {formatDecimal(bank.getBorrowRate().toNumber(), 2)}%
+                          </span>
+                        </p>
+                      </div>
+                      <div className="col-span-1">
+                        <p className="text-xs text-th-fgd-3">
+                          {t('utilization')}
+                        </p>
+                        <p className="font-mono text-th-fgd-1">
+                          {bank.uiDeposits() > 0
+                            ? formatDecimal(
+                                (bank.uiBorrows() / bank.uiDeposits()) * 100,
+                                1,
+                                { fixed: true }
+                              )
+                            : '0.0'}
+                          %
+                        </p>
+                      </div>
+                      <div className="col-span-1">
+                        <p className="text-xs text-th-fgd-3">
+                          {t('asset-weight')}
+                        </p>
+                        <p className="font-mono text-th-fgd-1">
+                          {bank.initAssetWeight.toFixed(2)}
+                        </p>
+                      </div>
+                      <div className="col-span-1">
+                        <p className="text-xs text-th-fgd-3">
+                          {t('liability-weight')}
+                        </p>
+                        <p className="font-mono text-th-fgd-1">
+                          {bank.initLiabWeight.toFixed(2)}
+                        </p>
+                      </div>
+                      <div className="col-span-1">
+                        <LinkButton
+                          className="flex items-center"
+                          onClick={() => goToTokenPage(bank)}
+                        >
+                          {t('token:token-details')}
+                          <ChevronRightIcon className="ml-2 h-5 w-5" />
+                        </LinkButton>
+                      </div>
+                    </div>
+                  </Transition>
+                </div>
+              )
+            })}
         </div>
       )}
     </ContentBox>
