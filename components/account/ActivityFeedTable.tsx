46e77374ee19dcbae7b142a5557aacbae5375444
saml33 <slam.uke@gmail.com>
2023-01-04 13:11:29 +1100

    check for mangoaccount in activity feed

--- components/account/ActivityFeedTable.tsx
+++ components/account/ActivityFeedTable.tsx
@@ -1,6 +1,5 @@
 import { EXPLORERS } from '@components/settings/PreferredExplorerSettings'
 import { IconButton, LinkButton } from '@components/shared/Button'
-import ConnectEmptyState from '@components/shared/ConnectEmptyState'
 import SheenLoader from '@components/shared/SheenLoader'
 import { Table, Td, Th, TrBody, TrHead } from '@components/shared/TableElements'
 import Tooltip from '@components/shared/Tooltip'
@@ -139,162 +138,152 @@ const ActivityFeedTable = ({
     return value
   }
 
-  return mangoAccount ? (
-    activityFeed.length || loadActivityFeed ? (
-      <>
-        {showTableView ? (
-          <Table className="min-w-full">
-            <thead className="sticky top-0 z-10">
-              <TrHead>
-                <Th className="bg-th-bkg-1 text-left">{t('date')}</Th>
-                <Th className="bg-th-bkg-1 text-right">
-                  {t('activity:activity')}
-                </Th>
-                <Th className="bg-th-bkg-1 text-right">
-                  {t('activity:credit')}
-                </Th>
-                <Th className="bg-th-bkg-1 text-right">
-                  {t('activity:debit')}
-                </Th>
-                <Th className="bg-th-bkg-1 text-right">
-                  {t('activity:activity-value')}
-                </Th>
-                <Th className="bg-th-bkg-1 text-right">{t('explorer')}</Th>
-              </TrHead>
-            </thead>
-            <tbody>
-              {activityFeed.map((activity: any) => {
-                const { activity_type, block_datetime } = activity
-                const { signature } = activity.activity_details
-                const isLiquidation =
-                  activity_type === 'liquidate_token_with_token'
-                const activityName = isLiquidation
-                  ? 'spot-liquidation'
-                  : activity_type
-                const amounts = getCreditAndDebit(activity)
-                const value = getValue(activity)
-                return (
-                  <TrBody
-                    key={signature}
-                    className={`default-transition text-sm hover:bg-th-bkg-2 ${
-                      isLiquidation ? 'cursor-pointer' : ''
+  return mangoAccount && (activityFeed.length || loadActivityFeed) ? (
+    <>
+      {showTableView ? (
+        <Table className="min-w-full">
+          <thead className="sticky top-0 z-10">
+            <TrHead>
+              <Th className="bg-th-bkg-1 text-left">{t('date')}</Th>
+              <Th className="bg-th-bkg-1 text-right">
+                {t('activity:activity')}
+              </Th>
+              <Th className="bg-th-bkg-1 text-right">{t('activity:credit')}</Th>
+              <Th className="bg-th-bkg-1 text-right">{t('activity:debit')}</Th>
+              <Th className="bg-th-bkg-1 text-right">
+                {t('activity:activity-value')}
+              </Th>
+              <Th className="bg-th-bkg-1 text-right">{t('explorer')}</Th>
+            </TrHead>
+          </thead>
+          <tbody>
+            {activityFeed.map((activity: any) => {
+              const { activity_type, block_datetime } = activity
+              const { signature } = activity.activity_details
+              const isLiquidation =
+                activity_type === 'liquidate_token_with_token'
+              const activityName = isLiquidation
+                ? 'spot-liquidation'
+                : activity_type
+              const amounts = getCreditAndDebit(activity)
+              const value = getValue(activity)
+              return (
+                <TrBody
+                  key={signature}
+                  className={`default-transition text-sm hover:bg-th-bkg-2 ${
+                    isLiquidation ? 'cursor-pointer' : ''
+                  }`}
+                  onClick={
+                    isLiquidation
+                      ? () => handleShowActivityDetails(activity)
+                      : undefined
+                  }
+                >
+                  <Td>
+                    <p className="font-body tracking-wide">
+                      {dayjs(block_datetime).format('ddd D MMM')}
+                    </p>
+                    <p className="text-xs text-th-fgd-3">
+                      {dayjs(block_datetime).format('h:mma')}
+                    </p>
+                  </Td>
+                  <Td className="text-right">
+                    {t(`activity:${activityName}`)}
+                  </Td>
+                  <Td className="text-right font-mono">
+                    {amounts.credit.value}{' '}
+                    <span className="font-body tracking-wide text-th-fgd-3">
+                      {amounts.credit.symbol}
+                    </span>
+                  </Td>
+                  <Td className="text-right font-mono">
+                    {amounts.debit.value}{' '}
+                    <span className="font-body tracking-wide text-th-fgd-3">
+                      {amounts.debit.symbol}
+                    </span>
+                  </Td>
+                  <Td
+                    className={`text-right font-mono ${
+                      activityName === 'swap' || activityName === 'perp_trade'
+                        ? 'text-th-fgd-2'
+                        : value >= 0
+                        ? 'text-th-up'
+                        : 'text-th-down'
                     }`}
-                    onClick={
-                      isLiquidation
-                        ? () => handleShowActivityDetails(activity)
-                        : undefined
-                    }
                   >
-                    <Td>
-                      <p className="font-body tracking-wide">
-                        {dayjs(block_datetime).format('ddd D MMM')}
-                      </p>
-                      <p className="text-xs text-th-fgd-3">
-                        {dayjs(block_datetime).format('h:mma')}
-                      </p>
-                    </Td>
-                    <Td className="text-right">
-                      {t(`activity:${activityName}`)}
-                    </Td>
-                    <Td className="text-right font-mono">
-                      {amounts.credit.value}{' '}
-                      <span className="font-body tracking-wide text-th-fgd-3">
-                        {amounts.credit.symbol}
-                      </span>
-                    </Td>
-                    <Td className="text-right font-mono">
-                      {amounts.debit.value}{' '}
-                      <span className="font-body tracking-wide text-th-fgd-3">
-                        {amounts.debit.symbol}
-                      </span>
-                    </Td>
-                    <Td
-                      className={`text-right font-mono ${
-                        activityName === 'swap' || activityName === 'perp_trade'
-                          ? 'text-th-fgd-2'
-                          : value >= 0
-                          ? 'text-th-up'
-                          : 'text-th-down'
-                      }`}
-                    >
-                      {value > 0 &&
-                      activityName !== 'swap' &&
-                      activityName !== 'perp_trade'
-                        ? '+'
-                        : ''}
-                      {formatFixedDecimals(value, true)}
-                    </Td>
-                    <Td>
-                      {activity_type !== 'liquidate_token_with_token' ? (
-                        <div className="flex items-center justify-end">
-                          <Tooltip
-                            content={`View on ${t(
-                              `settings:${preferredExplorer.name}`
-                            )}`}
-                            placement="top-end"
+                    {value > 0 &&
+                    activityName !== 'swap' &&
+                    activityName !== 'perp_trade'
+                      ? '+'
+                      : ''}
+                    {formatFixedDecimals(value, true)}
+                  </Td>
+                  <Td>
+                    {activity_type !== 'liquidate_token_with_token' ? (
+                      <div className="flex items-center justify-end">
+                        <Tooltip
+                          content={`View on ${t(
+                            `settings:${preferredExplorer.name}`
+                          )}`}
+                          placement="top-end"
+                        >
+                          <a
+                            href={`${preferredExplorer.url}${signature}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
                           >
-                            <a
-                              href={`${preferredExplorer.url}${signature}`}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <div className="h-6 w-6">
-                                <Image
-                                  alt=""
-                                  width="24"
-                                  height="24"
-                                  src={`/explorer-logos/${preferredExplorer.name}.png`}
-                                />
-                              </div>
-                            </a>
-                          </Tooltip>
-                        </div>
-                      ) : (
-                        <div className="flex items-center justify-end">
-                          <ChevronRightIcon className="h-6 w-6 text-th-fgd-3" />
-                        </div>
-                      )}
-                    </Td>
-                  </TrBody>
-                )
-              })}
-            </tbody>
-          </Table>
-        ) : (
-          <div>
-            {activityFeed.map((activity: any) => (
-              <MobileActivityFeedItem
-                activity={activity}
-                getValue={getValue}
-                key={activity.activity_details.signature}
-              />
-            ))}
-          </div>
-        )}
-        {loadActivityFeed ? (
-          <div className="mt-4 space-y-1.5">
-            {[...Array(4)].map((x, i) => (
-              <SheenLoader className="mx-4 flex flex-1 md:mx-6" key={i}>
-                <div className="h-16 w-full bg-th-bkg-2" />
-              </SheenLoader>
-            ))}
-          </div>
-        ) : null}
-        {activityFeed.length && activityFeed.length % 25 === 0 ? (
-          <div className="flex justify-center pt-6">
-            <LinkButton onClick={handleShowMore}>Show More</LinkButton>
-          </div>
-        ) : null}
-      </>
-    ) : (
-      <div className="flex flex-col items-center p-8">
-        <NoSymbolIcon className="mb-1 h-6 w-6 text-th-fgd-4" />
-        <p>{t('activity:no-activity')}</p>
-      </div>
-    )
+                            <div className="h-6 w-6">
+                              <Image
+                                alt=""
+                                width="24"
+                                height="24"
+                                src={`/explorer-logos/${preferredExplorer.name}.png`}
+                              />
+                            </div>
+                          </a>
+                        </Tooltip>
+                      </div>
+                    ) : (
+                      <div className="flex items-center justify-end">
+                        <ChevronRightIcon className="h-6 w-6 text-th-fgd-3" />
+                      </div>
+                    )}
+                  </Td>
+                </TrBody>
+              )
+            })}
+          </tbody>
+        </Table>
+      ) : (
+        <div>
+          {activityFeed.map((activity: any) => (
+            <MobileActivityFeedItem
+              activity={activity}
+              getValue={getValue}
+              key={activity.activity_details.signature}
+            />
+          ))}
+        </div>
+      )}
+      {loadActivityFeed ? (
+        <div className="mt-4 space-y-1.5">
+          {[...Array(4)].map((x, i) => (
+            <SheenLoader className="mx-4 flex flex-1 md:mx-6" key={i}>
+              <div className="h-16 w-full bg-th-bkg-2" />
+            </SheenLoader>
+          ))}
+        </div>
+      ) : null}
+      {activityFeed.length && activityFeed.length % 25 === 0 ? (
+        <div className="flex justify-center pt-6">
+          <LinkButton onClick={handleShowMore}>Show More</LinkButton>
+        </div>
+      ) : null}
+    </>
   ) : (
-    <div className="p-8">
-      <ConnectEmptyState text={t('activity:connect-activity')} />
+    <div className="flex flex-col items-center p-8">
+      <NoSymbolIcon className="mb-2 h-6 w-6 text-th-fgd-4" />
+      <p>{t('activity:no-activity')}</p>
     </div>
   )
 }
