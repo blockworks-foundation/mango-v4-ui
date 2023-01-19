270c74b9db35c1556f71908f18686c6cae1b8d26
saml33 <slam.uke@gmail.com>
2023-01-19 13:42:29 +1100

    toggle tradingview on mobile

--- components/trade/AdvancedMarketHeader.tsx
+++ components/trade/AdvancedMarketHeader.tsx
@@ -1,5 +1,7 @@
 import { PerpMarket } from '@blockworks-foundation/mango-v4'
+import { IconButton } from '@components/shared/Button'
 import Change from '@components/shared/Change'
+import { ChartBarIcon } from '@heroicons/react/20/solid'
 import { useCoingecko } from 'hooks/useCoingecko'
 import useSelectedMarket from 'hooks/useSelectedMarket'
 import { useTranslation } from 'next-i18next'
@@ -8,7 +10,13 @@ import { getDecimalCount } from 'utils/numbers'
 import MarketSelectDropdown from './MarketSelectDropdown'
 import PerpFundingRate from './PerpFundingRate'
 
-const AdvancedMarketHeader = () => {
+const AdvancedMarketHeader = ({
+  showChart,
+  setShowChart,
+}: {
+  showChart?: boolean
+  setShowChart?: (x: boolean) => void
+}) => {
   const { t } = useTranslation(['common', 'trade'])
   const { serumOrPerpMarket, baseSymbol, price } = useSelectedMarket()
   const { data: tokenPrices } = useCoingecko()
@@ -35,31 +43,46 @@ const AdvancedMarketHeader = () => {
       <div className=" w-full px-5 md:w-auto md:py-0 md:pr-6 lg:pb-0">
         <MarketSelectDropdown />
       </div>
-      <div className="flex items-center border-t border-th-bkg-3 py-2 px-5 md:border-t-0 md:py-0 md:px-0">
-        <div id="trade-step-two" className="flex-col md:ml-6">
-          <div className="text-xs text-th-fgd-4">{t('trade:oracle-price')}</div>
-          <div className="font-mono text-xs text-th-fgd-2">
-            {price ? (
-              `$${price.toFixed(
-                getDecimalCount(serumOrPerpMarket?.tickSize || 0.01)
-              )}`
-            ) : (
-              <span className="text-th-fgd-4">–</span>
-            )}
-          </div>
-        </div>
-        <div className="ml-6 flex-col">
-          <div className="text-xs text-th-fgd-4">{t('rolling-change')}</div>
-          <Change change={change} size="small" suffix="%" />
-        </div>
-        {serumOrPerpMarket instanceof PerpMarket ? (
-          <div className="ml-6 flex-col">
-            <div className="text-xs text-th-fgd-4">
-              {t('trade:funding-rate')}
+      <div className="border-t border-th-bkg-3 py-2 px-5 md:border-t-0 md:py-0 md:px-0">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <div id="trade-step-two" className="flex-col md:ml-6">
+              <div className="text-xs text-th-fgd-4">
+                {t('trade:oracle-price')}
+              </div>
+              <div className="font-mono text-xs text-th-fgd-2">
+                {price ? (
+                  `$${price.toFixed(
+                    getDecimalCount(serumOrPerpMarket?.tickSize || 0.01)
+                  )}`
+                ) : (
+                  <span className="text-th-fgd-4">–</span>
+                )}
+              </div>
             </div>
-            <PerpFundingRate />
+            <div className="ml-6 flex-col">
+              <div className="text-xs text-th-fgd-4">{t('rolling-change')}</div>
+              <Change change={change} size="small" suffix="%" />
+            </div>
+            {serumOrPerpMarket instanceof PerpMarket ? (
+              <div className="ml-6 flex-col">
+                <div className="text-xs text-th-fgd-4">
+                  {t('trade:funding-rate')}
+                </div>
+                <PerpFundingRate />
+              </div>
+            ) : null}
           </div>
-        ) : null}
+          {setShowChart ? (
+            <IconButton
+              className={showChart ? 'text-th-active' : 'text-th-fgd-2'}
+              onClick={() => setShowChart(!showChart)}
+              hideBg
+            >
+              <ChartBarIcon className="h-5 w-5" />
+            </IconButton>
+          ) : null}
+        </div>
       </div>
     </div>
   )
