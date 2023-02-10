0974463f00c04d360a27fbbab47e3c88c980e812
tjs <tjshipe@gmail.com>
2023-02-10 13:50:32 -0500

    tweak yaxis for chart data

--- components/shared/DetailedAreaChart.tsx
+++ components/shared/DetailedAreaChart.tsx
@@ -371,9 +371,9 @@ const DetailedAreaChart: FunctionComponent<DetailedAreaChartProps> = ({
                               if (difference < 0.1) {
                                 return [dataMin - 0.01, dataMax + 0.01]
                               } else if (difference < 1) {
-                                return [dataMin - 1, dataMax + 1]
+                                return [dataMin - 0.1, dataMax + 0.1]
                               } else if (difference < 10) {
-                                return [dataMin - 10, dataMax + 10]
+                                return [dataMin - 1, dataMax + 1]
                               } else {
                                 return [dataMin, dataMax]
                               }
