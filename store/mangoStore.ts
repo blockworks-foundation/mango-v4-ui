55db739d76662748989c4376388e0d74462454dc
tjs <tjshipe@gmail.com>
2023-03-08 12:53:48 -0500

    revert change to load last account and bump local storage key version

--- store/mangoStore.ts
+++ store/mangoStore.ts
@@ -627,7 +627,7 @@ const mangoStore = create<MangoStore>()(
               if (typeof lastAccount === 'string') {
                 try {
                   lastViewedAccount = mangoAccounts.find(
-                    (m) => m.publicKey.toString() === lastAccount
+                    (m) => m.publicKey.toString() === JSON.parse(lastAccount)
                   )
                 } catch (e) {
                   console.error('Error parsing last account', e)
