b312dca2e2087962313a3c2f3185a0c471a8a274
saml33 <slam.uke@gmail.com>
2023-03-28 13:46:22 +1100

    maintain activity feed scroll position

--- components/account/ActivityFeed.tsx
+++ components/account/ActivityFeed.tsx
@@ -1,5 +1,5 @@
 import mangoStore from '@store/mangoStore'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ActivityFeedTable from './ActivityFeedTable'
 import { LiquidationActivity } from 'types'
 import LiquidationDetails from './LiquidationDetails'
@@ -8,10 +8,18 @@ const ActivityFeed = () => {
   const activityFeed = mangoStore((s) => s.activityFeed.feed)
   const [showActivityDetail, setShowActivityDetail] =
     useState<LiquidationActivity>()
+  const [scrollPosition, setScrollPosition] = useState(0)
 
   const handleShowActivityDetails = (activity: LiquidationActivity) => {
     setShowActivityDetail(activity)
+    setScrollPosition(window.scrollY)
   }
+
+  useEffect(() => {
+    if (scrollPosition && !showActivityDetail) {
+      window.scroll(0, scrollPosition)
+    }
+  }, [scrollPosition, showActivityDetail])
 
   return !showActivityDetail ? (
     <ActivityFeedTable
