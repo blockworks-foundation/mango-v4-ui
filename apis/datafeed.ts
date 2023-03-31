a2b482cbe87f1740f202471c40550b4cd70c827d
tjs <tjshipe@gmail.com>
2023-03-31 16:29:06 -0400

    handle large decimals like the bonk chart

--- apis/datafeed.ts
+++ apis/datafeed.ts
@@ -223,7 +223,7 @@ export default {
       session: '24x7',
       timezone: 'Etc/UTC',
       minmov: 1,
-      pricescale: 100,
+      pricescale: 10 ** 16,
       has_intraday: true,
       has_weekly_and_monthly: false,
       has_empty_bars: true,
