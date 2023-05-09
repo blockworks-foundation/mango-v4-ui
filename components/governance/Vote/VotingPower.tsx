3dbd2997cd9f3766e14639caf3756cfe883096ae
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-05-09 17:56:45 +0200

    translations

--- components/governance/Vote/VotingPower.tsx
+++ components/governance/Vote/VotingPower.tsx
@@ -1,8 +1,6 @@
-import GovernanceDelegateModal from '@components/modals/governance/GovernanceDelegateModal'
-import Button from '@components/shared/Button'
+import GovernanceDelegate from '@components/governance/GovernanceDelegate'
 import GovernanceStore from '@store/governanceStore'
 import { useTranslation } from 'next-i18next'
-import { useState } from 'react'
 import { MANGO_MINT_DECIMALS } from 'utils/governance/constants'
 import { fmtTokenAmount } from 'utils/governance/tools'
 
@@ -11,27 +9,17 @@ const VotingPower = () => {
   const voter = GovernanceStore((s) => s.voter)
   const loadingVoter = GovernanceStore((s) => s.loadingVoter)
 
-  const [delegateModal, setDelegateModal] = useState(false)
-
   return (
-    <p className="whitespace-no-wrap mb-0.5 mt-2">
-      <Button
-        onClick={() => setDelegateModal(true)}
-        className="mr-4"
-        size="small"
-      >
-        {t('use-delegate')}
-      </Button>
-      <GovernanceDelegateModal
-        isOpen={delegateModal}
-        onClose={() => setDelegateModal(false)}
-      ></GovernanceDelegateModal>
-      {t('your-votes')}{' '}
-      <span className="font-mono text-th-fgd-2">
-        {!loadingVoter
-          ? fmtTokenAmount(voter.voteWeight, MANGO_MINT_DECIMALS)
-          : 0}
-      </span>
+    <p className="whitespace-no-wrap mb-0.5 mt-2 flex items-center">
+      <GovernanceDelegate></GovernanceDelegate>
+      <div className="ml-4 flex items-center">
+        {t('your-votes')}
+        <span className="font-mono text-th-fgd-2">
+          {!loadingVoter
+            ? fmtTokenAmount(voter.voteWeight, MANGO_MINT_DECIMALS)
+            : 0}
+        </span>
+      </div>
     </p>
   )
 }
