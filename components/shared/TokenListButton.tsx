1e10d799d0c4ca9ee8f8ef618fb603395eb4dd16
saml33 <slam.uke@gmail.com>
2023-05-15 14:45:46 +1000

    remove account modal double borders

--- components/shared/TokenListButton.tsx
+++ components/shared/TokenListButton.tsx
@@ -14,7 +14,7 @@ const TokenListButton = ({
   return (
     <button
       onClick={() => setShowList(true)}
-      className="flex h-full w-full items-center rounded-lg rounded-r-none border border-th-input-border bg-th-input-bkg py-2 px-3 text-th-fgd-2 hover:cursor-pointer hover:bg-th-bkg-2 hover:text-th-fgd-1 focus-visible:border-th-fgd-4"
+      className="flex h-full w-full items-center rounded-lg rounded-r-none border border-r-0 border-th-input-border bg-th-input-bkg py-2 px-3 text-th-fgd-2 focus-visible:bg-th-bkg-2 md:hover:cursor-pointer md:hover:bg-th-bkg-2 md:hover:text-th-fgd-1"
     >
       <div className="mr-2.5 flex min-w-[24px] items-center">
         <Image
