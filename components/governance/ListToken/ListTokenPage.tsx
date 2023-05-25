acdbbe29b085f7fa4951f80d7921f20286ab5ca0
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-05-25 15:59:45 +0200

    merge list token page

--- components/governance/ListToken/ListTokenPage.tsx
+++ components/governance/ListToken/ListTokenPage.tsx
@@ -1,13 +1,30 @@
 import dynamic from 'next/dynamic'
 import GovernancePageWrapper from '../GovernancePageWrapper'
+import { useState } from 'react'
+import Button from '@components/shared/Button'
+import ListMarket from '../ListMarket/ListMarket'
 
 const ListToken = dynamic(() => import('./ListToken'))
 
+enum LIST_OPTIONS {
+  MARKET,
+  TOKEN,
+}
+
 const ListTokenPage = () => {
+  const [listOptions, setListOption] = useState<LIST_OPTIONS | null>(null)
+
   return (
     <div className="p-8 pb-20 md:pb-16 lg:p-10">
       <GovernancePageWrapper>
-        <ListToken />
+        <Button onClick={() => setListOption(LIST_OPTIONS.MARKET)}>
+          List Market
+        </Button>
+        <Button onClick={() => setListOption(LIST_OPTIONS.TOKEN)}>
+          List Token
+        </Button>
+        {listOptions === LIST_OPTIONS.MARKET && <ListMarket></ListMarket>}
+        {listOptions === LIST_OPTIONS.TOKEN && <ListToken></ListToken>}
       </GovernancePageWrapper>
     </div>
   )
