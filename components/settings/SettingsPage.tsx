7aece6065383918df1899d2b322bcbdad52aceb9
saml33 <slam.uke@gmail.com>
2023-08-10 22:26:02 +1000

    hide account settings when no account

--- components/settings/SettingsPage.tsx
+++ components/settings/SettingsPage.tsx
@@ -8,18 +8,24 @@ import RpcSettings from './RpcSettings'
 import SoundSettings from './SoundSettings'
 import { breakpoints } from 'utils/theme'
 import AccountSettings from './AccountSettings'
+import useMangoAccount from 'hooks/useMangoAccount'
+import useUnownedAccount from 'hooks/useUnownedAccount'
 
 const SettingsPage = () => {
   const { width } = useViewport()
+  const { mangoAccountAddress } = useMangoAccount()
+  const { isUnownedAccount } = useUnownedAccount()
   const isMobile = width ? width < breakpoints.lg : false
   return (
     <div className="grid grid-cols-12">
       <div className="col-span-12 border-b border-th-bkg-3 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3">
         <RpcSettings />
       </div>
-      <div className="col-span-12 border-b border-th-bkg-3 pt-8 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3">
-        <AccountSettings />
-      </div>
+      {mangoAccountAddress && !isUnownedAccount ? (
+        <div className="col-span-12 border-b border-th-bkg-3 pt-8 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3">
+          <AccountSettings />
+        </div>
+      ) : null}
       <div className="col-span-12 border-b border-th-bkg-3 pt-8 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3">
         <DisplaySettings />
       </div>
