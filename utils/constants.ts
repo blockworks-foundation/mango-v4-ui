03c0fad2fabb0492d34f1ad58e9ff24e353749b0
saml33 <slam.uke@gmail.com>
2023-08-14 14:57:03 +1000

    add favoriting swap pairs

--- utils/constants.ts
+++ utils/constants.ts
@@ -45,6 +45,8 @@ export const TRADE_CHART_UI_KEY = 'tradeChart-0.3'
 
 export const FAVORITE_MARKETS_KEY = 'favoriteMarkets-0.2'
 
+export const FAVORITE_SWAPS_KEY = 'favoriteSwaps-0.1'
+
 export const THEME_KEY = 'theme-0.1'
 
 export const RPC_PROVIDER_KEY = 'rpcProviderKey-0.7'
