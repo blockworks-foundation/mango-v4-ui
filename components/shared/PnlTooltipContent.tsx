cf1580e3beda31684a012d5d5f3468f12766f81f
saml33 <slam.uke@gmail.com>
2023-08-29 12:27:35 +1000

    add roe to main perp positions table

--- components/shared/PnlTooltipContent.tsx
+++ components/shared/PnlTooltipContent.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from 'next-i18next'
 import { formatCurrencyValue } from 'utils/numbers'
-import FormatNumericValue from './FormatNumericValue'
 
 const getPnlColor = (pnl: number) => {
   return pnl < 0 ? 'text-th-down' : pnl > 0 ? 'text-th-up' : 'text-th-fgd-3'
@@ -11,13 +10,11 @@ const PnlTooltipContent = ({
   realizedPnl,
   totalPnl,
   unsettledPnl,
-  roe,
 }: {
   unrealizedPnl: number
   realizedPnl: number
   totalPnl: number
   unsettledPnl: number
-  roe: number
 }) => {
   const { t } = useTranslation(['common', 'trade'])
   return (
@@ -35,20 +32,13 @@ const PnlTooltipContent = ({
             {formatCurrencyValue(realizedPnl, 2)}
           </span>
         </div>
-        <div className="flex justify-between pt-1.5">
+        <div className="flex justify-between py-1.5">
           <p className="mr-3">{t('trade:total-pnl')}</p>
           <span className={`font-mono ${getPnlColor(totalPnl)}`}>
             {formatCurrencyValue(totalPnl, 2)}
           </span>
         </div>
-        <div className="flex justify-between border-b border-th-bkg-4 pb-3">
-          <p className="mr-3">{t('trade:return-on-equity')}</p>
-          <span className={`font-mono ${getPnlColor(roe)}`}>
-            <FormatNumericValue classNames="text-xs" value={roe} decimals={2} />
-            %
-          </span>
-        </div>
-        <div className="flex justify-between pt-1.5">
+        <div className="flex justify-between border-t border-th-bkg-4 pt-3">
           <p className="mr-3">
             {t('trade:unsettled')} {t('pnl')}
           </p>
