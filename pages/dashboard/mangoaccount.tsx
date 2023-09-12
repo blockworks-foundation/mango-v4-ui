b06cc1f0ae9402dd39f721b70fbe1f2f067189d5
microwavedcola1 <microwavedcola@gmail.com>
2023-09-12 09:35:47 +0200

    fix
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- pages/dashboard/mangoaccount.tsx
+++ pages/dashboard/mangoaccount.tsx
@@ -190,7 +190,7 @@ const MangoAccountDashboard: NextPage = () => {
                     />
                     <KeyValuePair
                       label="In Use Count"
-                      value={`$${token.inUseCount}`}
+                      value={`${token.inUseCount}`}
                     />
                   </div>
                 )
