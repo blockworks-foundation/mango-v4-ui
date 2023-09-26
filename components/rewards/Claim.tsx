77e46c99f0f2329d8c3cb5407580f2b3f6cbdfd8
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-09-26 16:46:47 +0200

    error handling

--- components/rewards/Claim.tsx
+++ components/rewards/Claim.tsx
@@ -177,7 +177,7 @@ const ClaimPage = () => {
     <div className="fixed bottom-0 left-0 right-0 top-0 z-[1000]">
       <RewardsComponent
         claims={claims}
-        setHide={setShowRender}
+        setShowRender={setShowRender}
       ></RewardsComponent>
     </div>
   ) : (
