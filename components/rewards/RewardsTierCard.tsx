78e6d3831ee6134a44ad341da77c5c2f063cf9cb
saml33 <slam.uke@gmail.com>
2023-09-27 21:55:56 +1000

    fix rewards tier card

--- components/rewards/RewardsTierCard.tsx
+++ components/rewards/RewardsTierCard.tsx
@@ -26,7 +26,7 @@ const RewardsTierCard = ({
     >
       <div className="flex items-center justify-between">
         <div className="flex items-center">
-          <div className="mr-4 flex h-14 w-14 flex-shrink-0 items-center justify-center rounded-full bg-th-bkg-1">
+          <div className="mr-4 flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-th-bkg-1">
             {icon}
           </div>
           <div>
@@ -34,9 +34,9 @@ const RewardsTierCard = ({
             <p className="rewards-p">{desc}</p>
           </div>
         </div>
-        <div className="flex items-center pl-4">
+        <div className="flex items-center space-x-2 pl-4">
           {status ? <Badge label={status} fillColor="bg-green-500" /> : null}
-          <ChevronRightIcon className="ml-4 h-6 w-6 text-th-fgd-3" />
+          <ChevronRightIcon className="h-6 w-6 text-th-fgd-3" />
         </div>
       </div>
     </button>
