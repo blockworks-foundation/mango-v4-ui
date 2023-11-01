1439e6544bf31b2209e21df0f4e48395884025d3
saml33 <slam.uke@gmail.com>
2023-11-01 14:48:12 +1100

    toggle cumulative values on stats charts

--- components/stats/mango/MangoPerpStatsCharts.tsx
+++ components/stats/mango/MangoPerpStatsCharts.tsx
@@ -4,26 +4,49 @@ import mangoStore from '@store/mangoStore'
 import { PerpStatsItem } from 'types'
 import DetailedAreaOrBarChart from '@components/shared/DetailedAreaOrBarChart'
 import { formatYAxis } from 'utils/formatting'
+import Switch from '@components/forms/Switch'
 
-interface OiValueItem {
+interface ValueItem {
   date: string
-  openInterest: number
-}
-
-interface FeeValueItem {
-  date: string
-  feeValue: number
-}
-
-interface VolumeValueItem {
-  date: string
-  volume: number
+  value: number
 }
 
 interface PerpStatsData {
-  feeValues: FeeValueItem[]
-  openInterestValues: OiValueItem[]
-  volumeValues: VolumeValueItem[]
+  feeValues: ValueItem[]
+  openInterestValues: ValueItem[]
+  volumeValues: ValueItem[]
+}
+
+interface GroupedDataItem extends ValueItem {
+  intervalStartMillis: number
+}
+
+const groupByHourlyInterval = (
+  data: ValueItem[],
+  intervalDurationHours: number,
+) => {
+  const intervalMillis = intervalDurationHours * 60 * 60 * 1000
+  const groupedData = []
+  let currentGroup: GroupedDataItem | null = null
+  for (let i = 0; i < data.length; i++) {
+    const obj = data[i]
+    const date = new Date(obj.date)
+    const intervalStartMillis =
+      Math.floor(date.getTime() / intervalMillis) * intervalMillis
+    if (
+      !currentGroup ||
+      currentGroup.intervalStartMillis !== intervalStartMillis
+    ) {
+      currentGroup = {
+        ...obj,
+        intervalStartMillis: intervalStartMillis,
+      }
+      groupedData.push(currentGroup)
+    } else {
+      currentGroup.value += obj.value
+    }
+  }
+  return groupedData
 }
 
 const MangoPerpStatsCharts = () => {
@@ -33,6 +56,8 @@ const MangoPerpStatsCharts = () => {
   const [feesDaysToShow, setFeesDaysToShow] = useState('30')
   const [oiDaysToShow, setOiDaysToShow] = useState('30')
   const [volumeDaysToShow, setVolumeDaysToShow] = useState('30')
+  const [showCumulativeFees, setShowCumulativeFees] = useState(true)
+  const [showCumulativeVolume, setShowCumulativeVolume] = useState(true)
 
   useEffect(() => {
     if (!perpStats || !perpStats.length) {
@@ -46,44 +71,42 @@ const MangoPerpStatsCharts = () => {
     const data = perpStats.reduce(
       (a: PerpStatsData, c: PerpStatsItem) => {
         const hasDateFee = a.feeValues.find(
-          (d: FeeValueItem) => d.date === c.date_hour,
+          (d: ValueItem) => d.date === c.date_hour,
         )
 
         const hasDateOpenInterest = a.openInterestValues.find(
-          (d: OiValueItem) => d.date === c.date_hour,
+          (d: ValueItem) => d.date === c.date_hour,
         )
 
         const hasDateVolume = a.volumeValues.find(
-          (d: VolumeValueItem) => d.date === c.date_hour,
+          (d: ValueItem) => d.date === c.date_hour,
         )
 
         if (!hasDateFee) {
           a.feeValues.push({
             date: c.date_hour,
-            feeValue: c.total_fees,
+            value: c.total_fees,
           })
         } else {
-          hasDateFee.feeValue += c.total_fees
+          hasDateFee.value += c.total_fees
         }
 
         if (!hasDateOpenInterest) {
           a.openInterestValues.push({
             date: c.date_hour,
-            openInterest: Math.floor(c.open_interest * c.price),
+            value: Math.floor(c.open_interest * c.price),
           })
         } else {
-          hasDateOpenInterest.openInterest += Math.floor(
-            c.open_interest * c.price,
-          )
+          hasDateOpenInterest.value += Math.floor(c.open_interest * c.price)
         }
 
         if (!hasDateVolume) {
           a.volumeValues.push({
             date: c.date_hour,
-            volume: c.cumulative_quote_volume,
+            value: c.cumulative_quote_volume,
           })
         } else {
-          hasDateVolume.volume += c.cumulative_quote_volume
+          hasDateVolume.value += c.cumulative_quote_volume
         }
 
         return a
@@ -103,31 +126,90 @@ const MangoPerpStatsCharts = () => {
       (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
     )
 
-    return [sortedFeeValues, sortedOpenInterestValues, sortedVolumeValues]
-  }, [perpStats])
+    let feeChartData = sortedFeeValues
+    if (!showCumulativeFees) {
+      const transformedData = []
+      for (let i = 1; i < sortedFeeValues.length; i++) {
+        const currentInterval = { ...sortedFeeValues[i] }
+        const previousInterval = sortedFeeValues[i - 1]
+
+        // Calculate the absolute fees for the current interval
+        currentInterval.value = currentInterval.value - previousInterval.value
+
+        transformedData.push(currentInterval)
+      }
+      transformedData.unshift(sortedFeeValues[0])
+
+      if (feesDaysToShow === '30') {
+        feeChartData = groupByHourlyInterval(transformedData, 24)
+      } else if (feesDaysToShow === '7') {
+        feeChartData = groupByHourlyInterval(transformedData, 4)
+      } else feeChartData = transformedData
+    }
+
+    let volumeChartData = sortedVolumeValues
+    if (!showCumulativeVolume) {
+      const transformedData = []
+      for (let i = 1; i < sortedVolumeValues.length; i++) {
+        const currentInterval = { ...sortedVolumeValues[i] }
+        const previousInterval = sortedVolumeValues[i - 1]
+
+        // Calculate the absolute fees for the current interval
+        currentInterval.value = currentInterval.value - previousInterval.value
+
+        transformedData.push(currentInterval)
+      }
+      transformedData.unshift(sortedVolumeValues[0])
+
+      if (volumeDaysToShow === '30') {
+        volumeChartData = groupByHourlyInterval(transformedData, 24)
+      } else if (volumeDaysToShow === '7') {
+        volumeChartData = groupByHourlyInterval(transformedData, 4)
+      } else volumeChartData = transformedData
+    }
+
+    return [feeChartData, sortedOpenInterestValues, volumeChartData]
+  }, [
+    feesDaysToShow,
+    perpStats,
+    showCumulativeFees,
+    showCumulativeVolume,
+    volumeDaysToShow,
+  ])
 
   return (
     <>
       {feeValues.length ? (
-        <div className="col-span-2 border-b border-th-bkg-3 px-6 py-4 md:col-span-1 md:pl-6">
-          <DetailedAreaOrBarChart
-            changeAsPercent
-            data={feeValues}
-            daysToShow={feesDaysToShow}
-            setDaysToShow={setFeesDaysToShow}
-            heightClass="h-64"
-            loading={loadingPerpStats}
-            loaderHeightClass="h-[350px]"
-            prefix="$"
-            tickFormat={(x) => `$${formatYAxis(x)}`}
-            title="Perp Fees"
-            xKey="date"
-            yKey={'feeValue'}
-          />
+        <div className="col-span-2 flex flex-col justify-between border-b border-th-bkg-3 md:col-span-1">
+          <div className="px-4 pt-4 md:px-6">
+            <DetailedAreaOrBarChart
+              changeAsPercent
+              data={feeValues}
+              daysToShow={feesDaysToShow}
+              setDaysToShow={setFeesDaysToShow}
+              heightClass="h-64"
+              loading={loadingPerpStats}
+              loaderHeightClass="h-[350px]"
+              prefix="$"
+              tickFormat={(x) => `$${formatYAxis(x)}`}
+              title="Perp Fees"
+              xKey="date"
+              yKey="value"
+            />
+          </div>
+          <div className="mt-2 flex justify-end border-t border-th-bkg-3 px-4 py-2 md:px-6">
+            <Switch
+              checked={showCumulativeFees}
+              onChange={() => setShowCumulativeFees(!showCumulativeFees)}
+              small
+            >
+              {t('stats:show-cumulative')}
+            </Switch>
+          </div>
         </div>
       ) : null}
       {openInterestValues.length ? (
-        <div className="col-span-2 border-b border-th-bkg-3 px-6 py-4 md:col-span-1 md:border-r">
+        <div className="col-span-2 border-b border-th-bkg-3 px-4 py-4 md:col-span-1 md:border-r md:px-6">
           <DetailedAreaOrBarChart
             changeAsPercent
             data={openInterestValues}
@@ -140,26 +222,37 @@ const MangoPerpStatsCharts = () => {
             tickFormat={(x) => `$${formatYAxis(x)}`}
             title={t('stats:perp-open-interest')}
             xKey="date"
-            yKey={'openInterest'}
+            yKey="value"
           />
         </div>
       ) : null}
       {volumeValues.length ? (
-        <div className="col-span-2 border-b border-th-bkg-3 px-6 py-4 md:col-span-1 md:pl-6">
-          <DetailedAreaOrBarChart
-            changeAsPercent
-            data={volumeValues}
-            daysToShow={volumeDaysToShow}
-            setDaysToShow={setVolumeDaysToShow}
-            heightClass="h-64"
-            loading={loadingPerpStats}
-            loaderHeightClass="h-[350px]"
-            prefix="$"
-            tickFormat={(x) => `$${formatYAxis(x)}`}
-            title={t('stats:perp-volume')}
-            xKey="date"
-            yKey={'volume'}
-          />
+        <div className="col-span-2 border-b border-th-bkg-3 md:col-span-1">
+          <div className="px-4 pt-4 md:px-6">
+            <DetailedAreaOrBarChart
+              changeAsPercent
+              data={volumeValues}
+              daysToShow={volumeDaysToShow}
+              setDaysToShow={setVolumeDaysToShow}
+              heightClass="h-64"
+              loading={loadingPerpStats}
+              loaderHeightClass="h-[350px]"
+              prefix="$"
+              tickFormat={(x) => `$${formatYAxis(x)}`}
+              title={t('stats:perp-volume')}
+              xKey="date"
+              yKey="value"
+            />
+          </div>
+          <div className="mt-2 flex justify-end border-t border-th-bkg-3 px-4 py-2 md:px-6">
+            <Switch
+              checked={showCumulativeVolume}
+              onChange={() => setShowCumulativeVolume(!showCumulativeVolume)}
+              small
+            >
+              {t('stats:show-cumulative')}
+            </Switch>
+          </div>
         </div>
       ) : null}
     </>
--- components/stats/mango/TokenStatsCharts.tsx
+++ components/stats/mango/TokenStatsCharts.tsx
@@ -1,11 +1,42 @@
 import mangoStore from '@store/mangoStore'
 import { useTranslation } from 'next-i18next'
 import { useEffect, useMemo, useState } from 'react'
-import dayjs from 'dayjs'
 import { formatYAxis } from 'utils/formatting'
-import useBanksWithBalances from 'hooks/useBanksWithBalances'
-import { toUiDecimals } from '@blockworks-foundation/mango-v4'
 import DetailedAreaOrBarChart from '@components/shared/DetailedAreaOrBarChart'
+import { MangoTokenStatsItem } from 'types'
+import Switch from '@components/forms/Switch'
+
+interface GroupedDataItem extends MangoTokenStatsItem {
+  intervalStartMillis: number
+}
+
+const groupByHourlyInterval = (
+  data: MangoTokenStatsItem[],
+  intervalDurationHours: number,
+) => {
+  const intervalMillis = intervalDurationHours * 60 * 60 * 1000
+  const groupedData = []
+  let currentGroup: GroupedDataItem | null = null
+  for (let i = 0; i < data.length; i++) {
+    const obj = data[i]
+    const date = new Date(obj.date)
+    const intervalStartMillis =
+      Math.floor(date.getTime() / intervalMillis) * intervalMillis
+    if (
+      !currentGroup ||
+      currentGroup.intervalStartMillis !== intervalStartMillis
+    ) {
+      currentGroup = {
+        ...obj,
+        intervalStartMillis: intervalStartMillis,
+      }
+      groupedData.push(currentGroup)
+    } else {
+      currentGroup.feesCollected += obj.feesCollected
+    }
+  }
+  return groupedData
+}
 
 const TokenStatsCharts = () => {
   const { t } = useTranslation(['common', 'token', 'trade'])
@@ -14,7 +45,7 @@ const TokenStatsCharts = () => {
   const [borrowDaysToShow, setBorrowDaysToShow] = useState('30')
   const [depositDaysToShow, setDepositDaysToShow] = useState('30')
   const [feesDaysToShow, setFeesDaysToShow] = useState('30')
-  const banks = useBanksWithBalances()
+  const [showCumulativeFees, setShowCumulativeFees] = useState(true)
   const tokenStatsInitialLoad = mangoStore((s) => s.tokenStats.initialLoad)
 
   useEffect(() => {
@@ -24,40 +55,38 @@ const TokenStatsCharts = () => {
     }
   }, [tokenStatsInitialLoad])
 
-  const [
-    currentTotalDepositValue,
-    currentTotalBorrowValue,
-    currentCollectedFeesValue,
-  ] = useMemo(() => {
-    if (banks.length) {
-      return [
-        banks.reduce((a, c) => a + c.bank.uiPrice * c.bank.uiDeposits(), 0),
-        banks.reduce((a, c) => a + c.bank.uiPrice * c.bank.uiBorrows(), 0),
-        banks.reduce(
-          (a, c) =>
-            a +
-            c.bank.uiPrice *
-              toUiDecimals(c.bank.collectedFeesNative, c.bank.mintDecimals),
-          0,
-        ),
-      ]
+  const tokenFeesChartData = useMemo(() => {
+    if (!mangoStats.length) return []
+    if (showCumulativeFees) {
+      return mangoStats
+    } else {
+      const transformedData = []
+      for (let i = 1; i < mangoStats.length; i++) {
+        const currentInterval = { ...mangoStats[i] }
+        const previousInterval = mangoStats[i - 1]
+
+        // Calculate the absolute fees for the current interval
+        currentInterval.feesCollected =
+          currentInterval.feesCollected - previousInterval.feesCollected
+
+        transformedData.push(currentInterval)
+      }
+      transformedData.unshift(mangoStats[0])
+
+      if (feesDaysToShow === '30') {
+        return groupByHourlyInterval(transformedData, 24)
+      } else if (feesDaysToShow === '7') {
+        return groupByHourlyInterval(transformedData, 4)
+      } else return transformedData
     }
-    return [0, 0, 0]
-  }, [banks])
+  }, [mangoStats, feesDaysToShow, showCumulativeFees])
 
   return (
     <>
       <div className="col-span-2 border-b border-th-bkg-3 px-6 py-4 md:col-span-1 md:border-r">
         <DetailedAreaOrBarChart
           changeAsPercent
-          data={mangoStats.concat([
-            {
-              date: dayjs().toISOString(),
-              depositValue: Math.floor(currentTotalDepositValue),
-              borrowValue: Math.floor(currentTotalBorrowValue),
-              feesCollected: currentCollectedFeesValue,
-            },
-          ])}
+          data={mangoStats}
           daysToShow={depositDaysToShow}
           setDaysToShow={setDepositDaysToShow}
           loading={loadingStats}
@@ -73,14 +102,7 @@ const TokenStatsCharts = () => {
       <div className="col-span-2 border-b border-th-bkg-3 px-6 py-4 md:col-span-1 md:pl-6">
         <DetailedAreaOrBarChart
           changeAsPercent
-          data={mangoStats.concat([
-            {
-              date: dayjs().toISOString(),
-              borrowValue: Math.floor(currentTotalBorrowValue),
-              depositValue: Math.floor(currentTotalDepositValue),
-              feesCollected: currentCollectedFeesValue,
-            },
-          ])}
+          data={mangoStats}
           daysToShow={borrowDaysToShow}
           setDaysToShow={setBorrowDaysToShow}
           heightClass="h-64"
@@ -93,29 +115,33 @@ const TokenStatsCharts = () => {
           yKey={'borrowValue'}
         />
       </div>
-      <div className="col-span-2 border-b border-th-bkg-3 px-6 py-4 md:col-span-1 md:border-r md:pl-6">
-        <DetailedAreaOrBarChart
-          changeAsPercent
-          data={mangoStats.concat([
-            {
-              date: dayjs().toISOString(),
-              borrowValue: Math.floor(currentTotalBorrowValue),
-              depositValue: Math.floor(currentTotalDepositValue),
-              feesCollected: currentCollectedFeesValue,
-            },
-          ])}
-          daysToShow={feesDaysToShow}
-          setDaysToShow={setFeesDaysToShow}
-          heightClass="h-64"
-          loaderHeightClass="h-[350px]"
-          loading={loadingStats}
-          prefix="$"
-          tickFormat={(x) => `$${formatYAxis(x)}`}
-          title={t('token:token-fees-collected')}
-          tooltipContent={t('token:tooltip-token-fees-collected')}
-          xKey="date"
-          yKey={'feesCollected'}
-        />
+      <div className="col-span-2 flex flex-col justify-between border-b border-th-bkg-3 md:col-span-1 md:border-r">
+        <div className="px-4 pt-4 md:px-6">
+          <DetailedAreaOrBarChart
+            changeAsPercent
+            data={tokenFeesChartData}
+            daysToShow={feesDaysToShow}
+            setDaysToShow={setFeesDaysToShow}
+            heightClass="h-64"
+            loaderHeightClass="h-[350px]"
+            loading={loadingStats}
+            prefix="$"
+            tickFormat={(x) => `$${formatYAxis(x)}`}
+            title={t('token:token-fees-collected')}
+            tooltipContent={t('token:tooltip-token-fees-collected')}
+            xKey="date"
+            yKey={'feesCollected'}
+          />
+        </div>
+        <div className="mt-2 flex justify-end border-t border-th-bkg-3 px-4 py-2 md:px-6">
+          <Switch
+            checked={showCumulativeFees}
+            onChange={() => setShowCumulativeFees(!showCumulativeFees)}
+            small
+          >
+            {t('stats:show-cumulative')}
+          </Switch>
+        </div>
       </div>
     </>
   )
