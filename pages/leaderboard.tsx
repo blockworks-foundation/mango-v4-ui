50fc5633cd7230c302ff326dc6d0f02b811b3cfa
saml33 <slam.uke@gmail.com>
2023-11-15 14:24:22 +1100

    add missing translation file

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -9,6 +9,7 @@ export async function getStaticProps({ locale }: { locale: string }) {
         'common',
         'leaderboard',
         'notifications',
+        'onboarding',
         'profile',
         'search',
         'settings',
--- pages/search.tsx
+++ pages/search.tsx
@@ -8,6 +8,7 @@ export async function getStaticProps({ locale }: { locale: string }) {
       ...(await serverSideTranslations(locale, [
         'common',
         'notifications',
+        'onboarding',
         'profile',
         'search',
         'settings',
