a882365765c6f397c842b12d992ca9a66ab36d00
tjs <tjshipe@gmail.com>
2023-11-21 17:35:43 -0500

    add waitForSlot to prevent stale rpc data

--- utils/network.ts
+++ utils/network.ts
@@ -1,3 +1,6 @@
+import { Connection } from '@solana/web3.js'
+import { sleep } from 'utils'
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export function getNetworkInfo() {
   const connection =
@@ -12,4 +15,12 @@ export function getNetworkInfo() {
 
 export default function isNetworkSlow() {
   return ['slow-2g', '2g', '3g'].includes(getNetworkInfo()) ? true : false
+}
+
+export async function waitForSlot(connection: Connection, slot: number) {
+  let lastSlot = 0
+  while (slot > lastSlot) {
+    lastSlot = await connection.getSlot()
+    await sleep(250)
+  }
 }
