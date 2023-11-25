47d69f14aabfeb5d29a7548a7fd69f4fb1bcb4be
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-11-25 01:24:04 +0100

    scale risk page

--- pages/dashboard/slippage.tsx
+++ pages/dashboard/slippage.tsx
@@ -5,6 +5,7 @@ import { DashboardNavbar } from '.'
 import { Table, Td, Th, TrBody, TrHead } from '@components/shared/TableElements'
 import { PublicKey } from '@solana/web3.js'
 import { formatNumericValue } from 'utils/numbers'
+import { toUiDecimals } from '@blockworks-foundation/mango-v4'
 
 export async function getStaticProps({ locale }: { locale: string }) {
   return {
@@ -26,7 +27,6 @@ const formatValue = (val: string | number | PublicKey) => {
 }
 const RiskDashboard: NextPage = () => {
   const { group } = useMangoGroup()
-
   return (
     <div className="grid grid-cols-12">
       <div className="col-span-12 lg:col-span-8 lg:col-start-3">
@@ -51,20 +51,72 @@ const RiskDashboard: NextPage = () => {
                       <Th xBorder className="text-left">
                         Amount
                       </Th>
-                      <Th>Avg PI %</Th>
-                      <Th>Min PI %</Th>
-                      <Th>Max PI %</Th>
-                      <Th>P90</Th>
-                      <Th>P95</Th>
+                      <Th xBorder>Avg PI %</Th>
+                      <Th xBorder>Min PI %</Th>
+                      <Th xBorder>Max PI %</Th>
+                      <Th xBorder>P90</Th>
+                      <Th xBorder>P95</Th>
                     </TrHead>
                   </thead>
                   <tbody>
                     {group.pis.map((row, idx: number) => {
                       return (
                         <TrBody key={idx}>
-                          {Object.values(row).map((val, valIdx) => {
+                          {Object.entries(row).map(([key, val], valIdx) => {
+                            //ETH
+                            const banks = group?.banksMapByName?.get(row.symbol)
+                            const bank = banks && banks[0]
+                            const uiBorrowWeightScaleStartQuote =
+                              bank &&
+                              toUiDecimals(bank.borrowWeightScaleStartQuote, 6)
+                            const uiDepositWeightScaleStartQuote =
+                              bank &&
+                              toUiDecimals(bank.depositWeightScaleStartQuote, 6)
+
+                            const keysForLiqFee = [
+                              'avg_price_impact_percent',
+                              'max_price_impact_percent',
+                              'min_price_impact_percent',
+                              'p95',
+                              'p90',
+                            ]
+
+                            //if liquidation fee is lower then price impact
+                            const isAboveLiqFee =
+                              keysForLiqFee.includes(key) &&
+                              bank &&
+                              typeof val === 'number' &&
+                              val > bank.liquidationFee.toNumber() * 100
+
+                            //if side is bid and borrowWeightScaleStartQuote is bigger then target_amount
+                            const isAmountBelowBorrowWeightScale =
+                              uiBorrowWeightScaleStartQuote === undefined ||
+                              (key === 'target_amount' &&
+                                typeof val === 'number' &&
+                                val < uiBorrowWeightScaleStartQuote &&
+                                row.side === 'bid')
+
+                            //if side is ask and borrowDepositWeightScaleStartQuote is bigger then target_amount
+                            const isAmountBelowDepositWeightScale =
+                              uiDepositWeightScaleStartQuote === undefined ||
+                              (key === 'target_amount' &&
+                                typeof val === 'number' &&
+                                val < uiDepositWeightScaleStartQuote &&
+                                row.side === 'ask')
+
+                            console.log(isAmountBelowBorrowWeightScale, '@@@@')
                             return (
-                              <Td xBorder key={valIdx}>
+                              <Td
+                                xBorder
+                                className={`${
+                                  isAboveLiqFee ||
+                                  isAmountBelowBorrowWeightScale ||
+                                  isAmountBelowDepositWeightScale
+                                    ? 'text-th-error'
+                                    : ''
+                                }`}
+                                key={valIdx}
+                              >
                                 {formatValue(val)}
                               </Td>
                             )
