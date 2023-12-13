1008060500e03833355505ccf57ee5f135b887db
microwavedcola1 <microwavedcola@gmail.com>
2023-12-13 09:56:53 +0100

    disable using api for now
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- store/mangoStore.ts
+++ store/mangoStore.ts
@@ -134,7 +134,7 @@ const initMangoClient = (
     prioritizationFee: opts.prioritizationFee,
     prependedGlobalAdditionalInstructions:
       opts.prependedGlobalAdditionalInstructions,
-    idsSource: 'api',
+    // idsSource: 'api', // disable for now, euroc listing seems to break this
     postSendTxCallback: ({ txid }: { txid: string }) => {
       if (telemetry) {
         telemetry('postSendTx', {
