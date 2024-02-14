a9e6a67f2f08cfd2604f8a2ebbe40f8fd169e73a
saml33 <slam.uke@gmail.com>
2024-02-14 12:08:16 +1100

    filter trade history for current market

--- utils/constants.ts
+++ utils/constants.ts
@@ -86,6 +86,7 @@ export const NEW_LISTING_BANNER_KEY = 'new-listing-banner-0.3'
 export const NON_RESTRICTED_JURISDICTION_KEY = 'non-restricted-jurisdiction-0.1'
 
 export const FILTER_ORDERS_FOR_MARKET_KEY = 'filterOrdersForMarket-0.1'
+export const FILTER_HISTORY_FOR_MARKET_KEY = 'filterHistoryForMarket-0.1'
 
 // Unused
 export const PROFILE_CATEGORIES = [
