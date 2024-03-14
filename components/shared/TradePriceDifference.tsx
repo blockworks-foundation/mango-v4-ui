66a313bef2832445ee96193d0a99c4abf8f2043d
saml33 <slam.uke@gmail.com>
2024-03-14 12:31:49 +1100

    add tooltip to limit price difference

--- components/shared/TradePriceDifference.tsx
+++ components/shared/TradePriceDifference.tsx
@@ -1,4 +1,6 @@
 import { useMemo } from 'react'
+import Tooltip from './Tooltip'
+import { useTranslation } from 'react-i18next'
 
 const TradePriceDifference = ({
   currentPrice,
@@ -7,6 +9,7 @@ const TradePriceDifference = ({
   currentPrice: number | undefined
   newPrice: number | undefined
 }) => {
+  const { t } = useTranslation('trade')
   const priceDifference = useMemo(() => {
     if (!currentPrice || !newPrice) return 0
     const difference = ((newPrice - currentPrice) / currentPrice) * 100
@@ -14,16 +17,18 @@ const TradePriceDifference = ({
   }, [currentPrice, newPrice])
 
   return (
-    <p
-      className={`font-mono text-xs ${
-        priceDifference >= 0 ? 'text-th-up' : 'text-th-down'
-      }`}
-    >
-      {priceDifference
-        ? (priceDifference > 0 ? '+' : '') + priceDifference.toFixed(2)
-        : '0.00'}
-      %
-    </p>
+    <Tooltip content={t('tooltip-limit-price-difference')}>
+      <p
+        className={`tooltip-underline font-mono text-xs ${
+          priceDifference >= 0 ? 'text-th-up' : 'text-th-down'
+        }`}
+      >
+        {priceDifference
+          ? (priceDifference > 0 ? '+' : '') + priceDifference.toFixed(2)
+          : '0.00'}
+        %
+      </p>
+    </Tooltip>
   )
 }
 
