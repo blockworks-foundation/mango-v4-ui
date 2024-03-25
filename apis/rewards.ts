73d8824478dcc48a4bcc4ad6f799a5d3e10a115a
Adrian Brzeziński <a.brzezinski94@gmail.com>
2024-03-25 16:41:46 +0100

    fix dist id

--- apis/rewards.ts
+++ apis/rewards.ts
@@ -3,7 +3,7 @@ import { Provider } from '@project-serum/anchor'
 import { MANGO_DATA_API_URL } from 'utils/constants'
 import { AnchorProvider } from '@coral-xyz/anchor'
 
-export const DISTRIBUTION_NUMBER_PREFIX = 131
+export const DISTRIBUTION_NUMBER_PREFIX = 132
 
 type AccountTier = {
   mango_account: string
